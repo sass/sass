df9fa2e681147168a220ed9ef23133efcd0c2596
nex3 <nex3@7063305b-7217-0410-af8c-cdc13e5119b9>
2006-11-16 05:48:19 +0000

    Fixed a problem with running helper_test.rb on its own.
    git-svn-id: svn://hamptoncatlin.com/haml/branches/edge@144 7063305b-7217-0410-af8c-cdc13e5119b9

--- test/helper_test.rb
+++ test/helper_test.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'test/unit'
-require File.dirname(__FILE__) + '/../lib/haml/helpers'
+require File.dirname(__FILE__) + '/../lib/haml/template'
 
 class HelperTest < Test::Unit::TestCase
   include Haml::Helpers
