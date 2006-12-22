23141baee5fb79d4a833461b8eccd0513e91deb9
nex3 <nex3@7063305b-7217-0410-af8c-cdc13e5119b9>
2006-12-22 05:11:03 +0000

    Nested attributes.
    git-svn-id: svn://hamptoncatlin.com/haml/trunk@240 7063305b-7217-0410-af8c-cdc13e5119b9

--- lib/sass/tree/attr_node.rb
+++ lib/sass/tree/attr_node.rb
@@ -11,8 +11,17 @@ module Sass::Tree
       super(value)
     end
     
-    def to_s
-      "#{name}: #{value};"
+    def to_s(parent_name = nil)
+      real_name = name
+      real_name = "#{parent_name}-#{real_name}" if parent_name
+      if children.size > 0
+        to_return = String.new
+        children.each { |kid| to_return += "#{kid.to_s(real_name)} " }
+        to_return[0...-1]
+      else
+        parent_name += '-' if parent_name
+        "#{real_name}: #{value};"
+      end
     end
   end
 end
