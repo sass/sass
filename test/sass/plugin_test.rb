f4209db126409a4bcf49ac652dedcca8caf7fa62
nex3 <nex3@7063305b-7217-0410-af8c-cdc13e5119b9>
2007-02-06 05:18:05 +0000

    Getting rid of a few extraneous tests.
    git-svn-id: svn://hamptoncatlin.com/haml/trunk@351 7063305b-7217-0410-af8c-cdc13e5119b9

--- test/sass/plugin_test.rb
+++ test/sass/plugin_test.rb
@@ -25,23 +25,6 @@ class SassPluginTest < Test::Unit::TestCase
     File.delete(*Dir[tempfile_loc('*')])
   end
 
-  def test_alternate_styles
-    engine = Sass::Engine.new(File.read(File.dirname(__FILE__) + '/templates/expanded.sass'),
-                              { :style => :expanded })
-    File.open(tempfile_loc('expanded'), 'w') { |f| f.write(engine.render) }
-    assert_renders_correctly('expanded')
-
-    engine = Sass::Engine.new(File.read(File.dirname(__FILE__) + '/templates/compact.sass'),
-                              { :style => :compact })
-    File.open(tempfile_loc('compact'), 'w') { |f| f.write(engine.render)}
-    assert_renders_correctly('compact')
-
-    engine = Sass::Engine.new(File.read(File.dirname(__FILE__) + '/templates/nested.sass'),
-                              { :style => :nested })
-    File.open(tempfile_loc('nested'), 'w') { |f| f.write(engine.render)}
-    assert_renders_correctly('nested')    
-  end
-
   def test_templates_should_render_correctly
     @@templates.each { |name| assert_renders_correctly(name) }
   end
