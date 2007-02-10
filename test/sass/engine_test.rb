c1a2ca3e48ddb82fed97c03d65020f47d0be8ae8
nex3 <nex3@7063305b-7217-0410-af8c-cdc13e5119b9>
2007-02-10 02:56:34 +0000

    Forgot to add the test for previous :p.
    git-svn-id: svn://hamptoncatlin.com/haml/trunk@359 7063305b-7217-0410-af8c-cdc13e5119b9

--- test/sass/engine_test.rb
+++ test/sass/engine_test.rb
@@ -33,7 +33,7 @@ class SassEngineTest < Test::Unit::TestCase
     "a\n :b c" => "Illegal Indentation: Only two space characters are allowed as tabulation.",
     "a\n    :b c" => "Illegal Indentation: Only two space characters are allowed as tabulation.",
     "a\n  :b\n  !c = 3" => "Constants may only be declared at the root of a document.",
-#    "!a = b c d" => 'Undefined operation: "b c d"'
+    "!a = b c d" => 'Undefined operation: "b c d"'
   }
   
   def test_basic_render
