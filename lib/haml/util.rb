63c8cf6ba55bf20f6f129fd5fc3de75edc6a38f7
nex3 <nex3@7063305b-7217-0410-af8c-cdc13e5119b9>
2007-03-26 05:33:41 +0000

    Don't really want to recursively merge arrays in the options hash.
    git-svn-id: svn://hamptoncatlin.com/haml/trunk@452 7063305b-7217-0410-af8c-cdc13e5119b9

--- lib/haml/util.rb
+++ lib/haml/util.rb
@@ -3,14 +3,12 @@
 
 class Hash # :nodoc:
   # Same as Hash#merge!,
-  # but recursively merges sub-hashes and arrays
+  # but recursively merges sub-hashes
   def rec_merge!(other)
     other.each do |key, value|
       myval = self[key]
       if value.is_a?(Hash) && myval.is_a?(Hash)
         myval.rec_merge!(value)
-      elsif value.is_a?(Array) && myval.is_a?(Array)
-        myval.concat(value)
       else
         self[key] = value
       end
