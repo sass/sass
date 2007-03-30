6fed7d3bce09e0ce7754ee081e244f63626e85a0
nex3 <nex3@7063305b-7217-0410-af8c-cdc13e5119b9>
2007-03-30 07:05:37 +0000

    Added Steve Ross' templated_mailer plugin, which allows Haml to be used as a template engine for ActionMailer, as an external.
    git-svn-id: svn://hamptoncatlin.com/haml/trunk@460 7063305b-7217-0410-af8c-cdc13e5119b9

--- init.rb
+++ init.rb
@@ -2,6 +2,8 @@ require 'haml'
 require 'haml/template'
 require 'sass'
 require 'sass/plugin'
+require 'templated_mailer/templated_mailer'
 
 ActionView::Base.register_template_handler('haml', Haml::Template)
+ActionMailer::Base.register_template_extension('haml')
 Sass::Plugin.update_stylesheets
