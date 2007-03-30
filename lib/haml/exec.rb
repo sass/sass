e145d319eb921f34546c5b534e8dfcad90b8d3e7
nex3 <nex3@7063305b-7217-0410-af8c-cdc13e5119b9>
2007-03-30 07:13:45 +0000

    Okay, that didn't work. Unfortunately, Rails' plugin installer won't install from svn:externals when it's pointed to an HTTP repository (or, rather, the Subversion http repository doesn't tell about its externals). Too bad.
    git-svn-id: svn://hamptoncatlin.com/haml/trunk@461 7063305b-7217-0410-af8c-cdc13e5119b9

--- lib/haml/exec.rb
+++ lib/haml/exec.rb
@@ -127,8 +127,16 @@ END
           end
 
           File.open(File.join(dir, 'init.rb'), 'w') do |file|
-            file.puts "require 'rubygems'\n" +
-                      File.read(File.join(File.dirname(__FILE__), '..', '..', 'init.rb'))
+            file.puts <<END
+require 'rubygems'
+require 'haml'
+require 'haml/template'
+require 'sass'
+require 'sass/plugin'
+
+ActionView::Base.register_template_handler('haml', Haml::Template)
+Sass::Plugin.update_stylesheets
+END
           end
 
           puts "Haml plugin added to #{original_dir}"
