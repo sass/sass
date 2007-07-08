77d09a5aea4a856be84b2f1fb6238d448bbd4701
hcatlin <hcatlin@7063305b-7217-0410-af8c-cdc13e5119b9>
2007-07-08 16:32:01 +0000

    adding in markaby benchmarks
    git-svn-id: svn://hamptoncatlin.com/haml/trunk@547 7063305b-7217-0410-af8c-cdc13e5119b9

--- test/benchmark.rb
+++ test/benchmark.rb
@@ -5,7 +5,9 @@ require 'rubygems'
 require 'active_support'
 require 'action_view'
 require 'benchmark'
+require 'markaby'
 require 'stringio'
+require 'open-uri'
 
 module Haml
   class Benchmarker
@@ -14,6 +16,7 @@ module Haml
     # directory.
     def initialize(base = File.dirname(__FILE__))
       ActionView::Base.register_template_handler("haml", Haml::Template)
+      ActionView::Base.register_template_handler("mab", Markaby::Template)
       unless base.class == ActionView::Base
         @base = ActionView::Base.new(base)
       else
@@ -29,19 +32,27 @@ module Haml
       template_name = 'standard'
       haml_template = "haml/templates/#{template_name}"
       rhtml_template = "haml/rhtml/#{template_name}"
+      markaby_template = File.dirname(__FILE__) + "/haml/markaby/#{template_name}.mab"
+      markaby_template_data = open(markaby_template).read
       sass_template = File.dirname(__FILE__) + "/sass/templates/complex.sass"
       
       old_stdout = $stdout
       $stdout = StringIO.new
-      
+
       times = Benchmark.bmbm do |b|
         b.report("haml:") { runs.times { @base.render haml_template } }
         b.report("erb:") { runs.times { @base.render rhtml_template } }
+        b.report("mab:") { runs.times { 
+          Markaby::Template.new(markaby_template_data).render
+          #@base.render markaby_template 
+        } }
       end
       
       #puts times[0].inspect, times[1].inspect
       ratio = sprintf("%g", times[0].to_a[5] / times[1].to_a[5])
+      mab_ratio = sprintf("%g", times[2].to_a[5] / times[1].to_a[5])
       puts "Haml/ERB: " + ratio
+      puts "Mab/Haml: " + mab_ratio
       
       puts '', '-' * 50, 'Sass on its own', '-' * 50
       
