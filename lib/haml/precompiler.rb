e247a9bde630b3cee0bf5dde44a2fc2a8602ff1c
nex3 <nex3@7063305b-7217-0410-af8c-cdc13e5119b9>
2008-01-02 01:50:55 +0000

    Haml _should_ work with both 1.8 and 1.9 now. It hasn't been fully tested, though. Patch by Aman Gupta.
    git-svn-id: svn://hamptoncatlin.com/haml/trunk@706 7063305b-7217-0410-af8c-cdc13e5119b9

--- lib/haml/precompiler.rb
+++ lib/haml/precompiler.rb
@@ -122,7 +122,7 @@ END
       @tab_change  = 0
 
       old_line = Line.new
-      (@template + "\n-#").each_with_index do |text, index|
+      (@template + "\n-#").split("\n").each_with_index do |text, index|
         line = Line.new text.strip, text.lstrip.chomp, index
         line.spaces, line.tabs = count_soft_tabs(text)
 
