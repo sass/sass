d1cc354a6d3fa4cfd722226d796c113472067a35
Nathan Weizenbaum <nex342@gmail.com>
2008-03-30 08:59:05 -0700

    Add failing tests for proper multiline selector formatting.

--- test/sass/engine_test.rb
+++ test/sass/engine_test.rb
@@ -40,6 +40,8 @@ class SassEngineTest < Test::Unit::TestCase
     "& a\n  :b c" => "Base-level rules cannot contain the parent-selector-referencing character '&'",
     "a\n  :b\n    c" => "Illegal nesting: Only attributes may be nested beneath attributes.",
     "a,\n  :b c" => "Rules can\'t end in commas.",
+    "a," => "Rules can\'t end in commas.",
+    "a,\n!b = c" => "Rules can\'t end in commas.",
     "!a = b\n  :c d\n" => "Illegal nesting: Nothing may be nested beneath constants.",
     "@import foo.sass" => "File to import not found or unreadable: foo.sass",
     "@import templates/basic\n  foo" => "Illegal nesting: Nothing may be nested beneath import directives.",
@@ -114,10 +116,12 @@ class SassEngineTest < Test::Unit::TestCase
   end
 
   def test_basic_multiline_selector
-    assert_equal("#foo #bar, #baz #boom {\n  foo: bar; }\n",
+    assert_equal("#foo #bar,\n#baz #boom {\n  foo: bar; }\n",
                  render("#foo #bar,\n#baz #boom\n  :foo bar"))
-    assert_equal("#foo #bar, #foo #baz {\n  foo: bar; }\n",
+    assert_equal("#foo #bar,\n#foo #baz {\n  foo: bar; }\n",
                  render("#foo\n  #bar,\n  #baz\n    :foo bar"))
+    assert_equal("#foo,\n#bar {\n  foo: bar; }\n  #foo #baz,\n  #bar #baz {\n    foo: bar; }\n",
+                 render("#foo,\n#bar\n  :foo bar\n  #baz\n    :foo bar"))
     assert_equal("#foo #bar, #baz #boom { foo: bar; }\n",
                  render("#foo #bar,\n#baz #boom\n  :foo bar", :style => :compact))
                  
@@ -184,11 +188,11 @@ class SassEngineTest < Test::Unit::TestCase
     assert_equal("@a{#b{a:b}#b #c{d:e}}\n",
                  render("@a\n  #b\n    :a b\n    #c\n      :d e", :style => :compressed))
                  
-    assert_equal("@a {\n  #foo, #bar {\n    b: c; } }\n",
+    assert_equal("@a {\n  #foo,\n  #bar {\n    b: c; } }\n",
                  render("@a\n  #foo, \n  #bar\n    :b c"))
     assert_equal("@a { #foo, #bar { b: c; } }\n",
                  render("@a\n  #foo, \n  #bar\n    :b c", :style => :compact))
-    assert_equal("@a {\n  #foo, #bar {\n    b: c;\n  }\n}\n",
+    assert_equal("@a {\n  #foo,\n  #bar {\n    b: c;\n  }\n}\n",
                  render("@a\n  #foo, \n  #bar\n    :b c", :style => :expanded))
     assert_equal("@a{#foo,#bar{b:c}}\n",
                  render("@a\n  #foo, \n  #bar\n    :b c", :style => :compressed))
