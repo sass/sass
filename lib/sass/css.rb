2a436fbf9233fa10b866f80ae8911dc296a99093
Chris Eppstein <chris@eppsteins.net>
2008-11-22 09:30:19 -0800

    Fixed RuleNode initializer bug in css2sass.

--- lib/sass/css.rb
+++ lib/sass/css.rb
@@ -237,7 +237,7 @@ module Sass
       root.children.map! do |child|
         next child unless Tree::RuleNode === child && child.rule.include?(',')
         child.rule.split(',').map do |rule|
-          node = Tree::RuleNode.new(rule.strip, nil)
+          node = Tree::RuleNode.new(rule.strip, {})
           node.children = child.children
           node
         end
