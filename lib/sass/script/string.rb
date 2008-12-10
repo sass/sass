f1b3b649f0ae61c842d9163d2d317e2110fd91e5
Chris Eppstein <chris@eppsteins.net>
2008-12-10 01:01:59 -0800

    Implement String interpolation.

--- lib/sass/script/string.rb
+++ lib/sass/script/string.rb
@@ -2,6 +2,15 @@ require 'sass/script/literal'
 
 module Sass::Script
   class String < Literal # :nodoc:
+    INTERPOLATION = /(^|[^\\])\#\{([^}]*)\}/
+    #TODO pass line & char context to perform
+    def perform(environment)
+      interpolated = @value.gsub(INTERPOLATION) do |match|
+        "#{$1}#{Sass::Script.resolve($2, 0, 0, environment)}"
+      end
+      Sass::Script::String.new(interpolated)
+    end
+
     def plus(other)
       Sass::Script::String.new(self.to_s + other.to_s)
     end
