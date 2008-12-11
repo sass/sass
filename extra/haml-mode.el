0f39b4f25a41a6f0de7a9cdab9cbc773e4768b0d
Nathan Weizenbaum <nex342@gmail.com>
2008-12-11 15:13:24 -0800

    [Emacs] Add support for comments.

--- extra/haml-mode.el
+++ extra/haml-mode.el
@@ -149,6 +149,7 @@ text nested beneath them.")
   (set (make-local-variable 'indent-line-function) 'haml-indent-line)
   (set (make-local-variable 'indent-region-function) 'haml-indent-region)
   (set (make-local-variable 'parse-sexp-lookup-properties) t)
+  (setq comment-start "-#")
   (setq indent-tabs-mode nil)
   (setq font-lock-defaults '((haml-font-lock-keywords) nil t)))
 
