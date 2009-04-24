cfdde37745f0bad31c5a8a6503b0753969833e32
Nathan Weizenbaum <nex342@gmail.com>
2009-04-24 13:48:42 -0700

    [Sass] Convert Sass::Script::Operation docs to YARD.

--- lib/sass/script/operation.rb
+++ lib/sass/script/operation.rb
@@ -1,3 +1,4 @@
+require 'set'
 require 'sass/script/string'
 require 'sass/script/number'
 require 'sass/script/color'
@@ -5,17 +6,29 @@ require 'sass/script/functions'
 require 'sass/script/unary_operation'
 
 module Sass::Script
-  class Operation # :nodoc:
+  # A SassScript node representing a binary operation,
+  # such as `!a + !b` or `"foo" + 1`.
+  class Operation
+    # @param operand1 [#perform(Sass::Environment)] A script node
+    # @param operand2 [#perform(Sass::Environment)] A script node
+    # @param operator [Symbol] The operator to perform.
+    #   This should be one of the binary operator names in {Lexer::OPERATORS}
     def initialize(operand1, operand2, operator)
       @operand1 = operand1
       @operand2 = operand2
       @operator = operator
     end
 
+    # @return [String] A human-readable s-expression representation of the operation
     def inspect
       "(#{@operator.inspect} #{@operand1.inspect} #{@operand2.inspect})"
     end
 
+    # Evaluates the operation.
+    #
+    # @param environment [Sass::Environment] The environment in which to evaluate the SassScript
+    # @return [Literal] The SassScript object that is the value of the operation
+    # @raise [Sass::SyntaxError] if the operation is undefined for the operands
     def perform(environment)
       literal1 = @operand1.perform(environment)
       literal2 = @operand2.perform(environment)
