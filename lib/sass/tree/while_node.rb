e78c80c4b118a6d28ec8b3dba96d78d6785fa2c8
Nathan Weizenbaum <nex342@gmail.com>
2009-04-25 02:24:27 -0700

    [Sass] Convert Sass::Tree::WhileNode docs to YARD.

--- lib/sass/tree/while_node.rb
+++ lib/sass/tree/while_node.rb
@@ -1,14 +1,26 @@
 require 'sass/tree/node'
 
 module Sass::Tree
+  # A dynamic node representing a Sass `@while` loop.
+  #
+  # @see Sass::Tree
   class WhileNode < Node
+    # @param expr [Script::Node] The parse tree for the continue expression
+    # @param options [Hash<Symbol, Object>] An options hash;
+    #   see [the Sass options documentation](../../Sass.html#sass_options)
     def initialize(expr, options)
       @expr = expr
       super(options)
     end
 
-    private
+    protected
 
+    # Runs the child nodes until the continue expression becomes false.
+    #
+    # @param environment [Sass::Environment] The lexical environment containing
+    #   variable and mixin values
+    # @return [Array<Tree::Node>] The resulting static nodes
+    # @see Sass::Tree
     def _perform(environment)
       children = []
       new_environment = Sass::Environment.new(environment)
