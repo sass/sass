8b4233e36df9da19e3ad0903c13bf8a1340b5f8e
Nathan Weizenbaum <nex342@gmail.com>
2009-04-30 13:26:51 -0700

    [Haml] Convert Haml::Shared docs to YARD.

--- lib/haml/shared.rb
+++ lib/haml/shared.rb
@@ -1,17 +1,43 @@
 require 'strscan'
 
-# :stopdoc:
 module Haml
-  # This module contains functionality that's shared across Haml and Sass.
+  # This module contains functionality that's shared between Haml and Sass.
   module Shared
     extend self
 
+    # Scans through a string looking for the interoplation-opening `#{`
+    # and, when it's found, yields the scanner to the calling code
+    # so it can handle it properly.
+    #
+    # The scanner will have any backslashes immediately in front of the `#{`
+    # as the second capture group (`scan[2]`),
+    # and the text prior to that as the first (`scan[1]`).
+    #
+    # @yieldparam scan [StringScanner] The scanner scanning through the string
+    # @return [String] The text remaining in the scanner after all `#{`s have been processed
     def handle_interpolation(str)
       scan = StringScanner.new(str)
       yield scan while scan.scan(/(.*?)(\\*)\#\{/)
       scan.rest
     end
 
+    # Moves a scanner through a balanced pair of characters.
+    # For example:
+    #
+    #     Foo (Bar (Baz bang) bop) (Bang (bop bip))
+    #     ^                       ^
+    #     from                    to
+    #
+    # @param scanner [StringScanner] The string scanner to move
+    # @param start [Character] The character opening the balanced pair.
+    #   A `Fixnum` in 1.8, a `String` in 1.9
+    # @param finish [Character] The character closing the balanced pair.
+    #   A `Fixnum` in 1.8, a `String` in 1.9
+    # @param count [Fixnum] The number of opening characters matched
+    #   before calling this method
+    # @return [(String, String)] The string matched within the balanced pair
+    #   and the rest of the string.
+    #   `["Foo (Bar (Baz bang) bop)", " (Bang (bop bip))"]` in the example above.
     def balance(scanner, start, finish, count = 0)
       str = ''
       scanner = StringScanner.new(scanner) unless scanner.is_a? StringScanner
@@ -24,6 +50,12 @@ module Haml
       end
     end
 
+    # Formats a string for use in error messages about indentation.
+    #
+    # @param indentation [String] The string used for indentation
+    # @param was [Boolean] Whether or not to add `"was"` or `"were"`
+    #   (depending on how many characters were in `indentation`)
+    # @return [String] The name of the indentation (e.g. `"12 spaces"`, `"1 tab"`)
     def human_indentation(indentation, was = false)
       if !indentation.include?(?\t)
         noun = 'space'
@@ -44,4 +76,3 @@ module Haml
     end
   end
 end
-# :startdoc:
