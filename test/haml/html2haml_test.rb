6bd1ee3bee61445173f09cba72d078824eb59f19
Nathan Weizenbaum <nex342@gmail.com>
2009-10-04 21:07:57 -0700

    [Haml] [html2haml] Clean up some style issues in html2haml_test.

--- test/haml/html2haml_test.rb
+++ test/haml/html2haml_test.rb
@@ -3,7 +3,6 @@ require File.dirname(__FILE__) + '/../test_helper'
 require 'haml/html'
 
 class Html2HamlTest < Test::Unit::TestCase
-
   def test_empty_render_should_remain_empty
     assert_equal '', render('')
   end
@@ -40,53 +39,6 @@ class Html2HamlTest < Test::Unit::TestCase
     assert_equal('%p{ :foo => "\#{bar} baz" }', render('<p foo="#{bar} baz"></p>'))
   end
 
-  def test_rhtml
-    assert_equal '- foo = bar', render_rhtml('<% foo = bar %>')
-    assert_equal '- foo = bar', render_rhtml('<% foo = bar -%>')
-    assert_equal '= h @item.title', render_rhtml('<%=h @item.title %>')
-    assert_equal '= h @item.title', render_rhtml('<%=h @item.title -%>')
-  end
-  
-  def test_rhtml_with_html_special_chars
-    assert_equal '= 3 < 5 ? "OK" : "Your computer is b0rken"',
-      render_rhtml(%Q{<%= 3 < 5 ? "OK" : "Your computer is b0rken" %>})
-  end
-  
-  def test_rhtml_in_class_attribute
-    assert_equal "%div{ :class => dyna_class }\n  I have a dynamic attribute",
-      render_rhtml(%Q{<div class="<%= dyna_class %>">I have a dynamic attribute</div>})
-  end
-  
-  def test_rhtml_in_id_attribute
-    assert_equal "%div{ :id => dyna_id }\n  I have a dynamic attribute",
-      render_rhtml(%Q{<div id="<%= dyna_id %>">I have a dynamic attribute</div>})
-  end
-  
-  def test_rhtml_in_attribute_results_in_string_interpolation
-    assert_equal %(%div{ :id => "item_\#{i}" }\n  Ruby string interpolation FTW),
-      render_rhtml(%Q{<div id="item_<%= i %>">Ruby string interpolation FTW</div>})
-  end
-  
-  def test_rhtml_in_attribute_with_trailing_content
-    assert_equal %(%div{ :class => "\#{12}!" }\n  Bang!),
-      render_rhtml(%Q{<div class="<%= 12 %>!">Bang!</div>})
-  end
-  
-  def test_rhtml_in_html_escaped_attribute
-    assert_equal %(%div{ :class => "foo" }\n  Bang!),
-      render_rhtml(%Q{<div class="<%= "foo" %>">Bang!</div>})
-  end
-  
-  def test_rhtml_in_attribute_to_multiple_interpolations
-    assert_equal %(%div{ :class => "\#{12} + \#{13}" }\n  Math is super),
-      render_rhtml(%Q{<div class="<%= 12 %> + <%= 13 %>">Math is super</div>})
-  end
-  
-  def test_whitespace_eating_erb_tags
-    assert_equal %(- form_for),
-      render_rhtml(%Q{<%- form_for -%>})
-  end
-
   def test_cdata
     assert_equal(<<HAML.strip, render(<<HTML))
 %p
@@ -101,6 +53,64 @@ HAML
   </a>
 ]]></p>
 HTML
+  end
+
+  def test_rhtml
+    assert_equal '- foo = bar', render_rhtml('<% foo = bar %>')
+    assert_equal '- foo = bar', render_rhtml('<% foo = bar -%>')
+    assert_equal '= h @item.title', render_rhtml('<%=h @item.title %>')
+    assert_equal '= h @item.title', render_rhtml('<%=h @item.title -%>')
+  end
+
+  def test_rhtml_with_html_special_chars
+    assert_equal '= 3 < 5 ? "OK" : "Your computer is b0rken"',
+      render_rhtml('<%= 3 < 5 ? "OK" : "Your computer is b0rken" %>')
+  end
+
+  def test_rhtml_in_class_attribute
+    assert_equal "%div{ :class => dyna_class }\n  I have a dynamic attribute",
+      render_rhtml('<div class="<%= dyna_class %>">I have a dynamic attribute</div>')
+  end
+
+  def test_rhtml_in_id_attribute
+    assert_equal "%div{ :id => dyna_id }\n  I have a dynamic attribute",
+      render_rhtml('<div id="<%= dyna_id %>">I have a dynamic attribute</div>')
+  end
+
+  def test_rhtml_in_attribute_results_in_string_interpolation
+    assert_equal(<<HAML.rstrip, render_rhtml(<<ERB))
+%div{ :id => "item_\#{i}" }
+  Ruby string interpolation FTW
+HAML
+<div id="item_<%= i %>">Ruby string interpolation FTW</div>
+ERB
+  end
+
+  def test_rhtml_in_attribute_with_trailing_content
+    assert_equal(<<HAML.rstrip, render_rhtml(<<ERB))
+%div{ :class => "\#{12}!" }
+  Bang!
+HAML
+<div class="<%= 12 %>!">Bang!</div>
+ERB
+  end
+
+  def test_rhtml_in_html_escaped_attribute
+    assert_equal %(%div{ :class => "foo" }\n  Bang!),
+      render_rhtml(%Q{<div class="<%= "foo" %>">Bang!</div>})
+  end
+
+  def test_rhtml_in_attribute_to_multiple_interpolations
+    assert_equal(<<HAML.rstrip, render_rhtml(<<ERB))
+%div{ :class => "\#{12} + \#{13}" }
+  Math is super
+HAML
+<div class="<%= 12 %> + <%= 13 %>">Math is super</div>
+ERB
+  end
+
+  def test_whitespace_eating_erb_tags
+    assert_equal '- form_for', render_rhtml('<%- form_for -%>')
   end
 
   def test_interpolation_in_rhtml
