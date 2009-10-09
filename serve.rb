d3e005e46033480567e57e55520cb6d505214976
Nathan Weizenbaum <nex342@gmail.com>
2009-10-08 18:49:04 -0700

    Add a timeout for the labs rendering.

--- serve.rb
+++ serve.rb
@@ -3,14 +3,19 @@ require 'rubygems'
 gem 'sinatra-sinatra'
 require 'sinatra'
 require 'sinatra/dynamicmatic'
+require 'timeout'
 
 get('/try.html') {haml :try}
 
 post('/try.html') do
   begin
-    @result = Haml::Engine.new(params[:input], :suppress_eval => true).render
+    Timeout.timeout(5) do
+      @result = Haml::Engine.new(params[:input], :suppress_eval => true).render
+    end
   rescue Haml::SyntaxError => e
     @result = "Haml Error: " + e
+  rescue Timeout::Error
+    @result = "Timed out!"
   end
 
   haml :try
