b3a890695a5a34f868ee72e11781fc2e19b2d85e
Nathan Weizenbaum <nex342@gmail.com>
2009-10-23 02:56:00 -0700

    [Haml] [html2haml] Properly document the block detection implementation stuff.

--- lib/haml/html.rb
+++ lib/haml/html.rb
@@ -98,6 +98,9 @@ require 'hpricot'
 module Haml
   # Converts HTML documents into Haml templates.
   # Depends on [Hpricot](http://code.whytheluckystiff.net/hpricot/) for HTML parsing.
+  # If ERB conversion is being used, also depends on 
+  # [Erubis](http://www.kuwata-lab.com/erubis) to parse the ERB
+  # and [ruby_parser](http://parsetree.rubyforge.org/) to parse the Ruby code.
   #
   # Example usage:
   #
