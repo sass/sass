5caec5341abf1a223b3480c6f84401de81b5c9f3
Nathan Weizenbaum <nex342@gmail.com>
2009-11-09 18:09:20 -0800

    [Sass] Document Plugin::Rack.

--- lib/sass/plugin/rack.rb
+++ lib/sass/plugin/rack.rb
@@ -2,11 +2,42 @@ require 'sass/plugin'
 
 module Sass
   module Plugin
+    # Rack middleware for compiling Sass code.
+    #
+    # ## Activate
+    #
+    #     require 'sass/plugin/rack'
+    #     use Sass::Plugin::Rack
+    #
+    # ## Customize
+    #
+    #     Sass::Plugin.options.merge(
+    #       :cache_location => './tmp/sass-cache',
+    #       :never_update => environment != :production,
+    #       :full_exception => environment != :production)
+    #
+    # {file:SASS_REFERENCE.md#options See the Reference for more options}.
+    #
+    # ## Use
+    #
+    # Put your Sass files in `public/stylesheets/sass`.
+    # Your CSS will be generated in `public/stylesheets`,
+    # and regenerated every request if necessary.
+    # The locations and frequency {file:SASS_REFERENCE.md#options can be customized}.
+    # That's all there is to it!
     class Rack
+      # Initialize the middleware.
+      #
+      # @param app [#call] The Rack application
       def initialize(app)
         @app = app
       end
 
+      # Process a request, checking the Sass stylesheets for changes
+      # and updating them if necessary.
+      #
+      # @param env The Rack request environment
+      # @return [(#to_i, Hash<String, String>, Object)] The Rack response
       def call(env)
         Sass::Plugin.check_for_updates
         @app.call(env)
