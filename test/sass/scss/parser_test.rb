df98bc74eef61a4a24434d534c48f3d0e84f4c90
Nathan Weizenbaum <nex342@gmail.com>
2009-12-27 17:15:52 -0800

    [Sass] [SCSS] Add a test for invalid directives.

--- test/sass/scss/parser_test.rb
+++ test/sass/scss/parser_test.rb
@@ -440,6 +440,13 @@ SCSS
     assert_selector_parses("E, F\nG, H")
   end
 
+  ## Errors
+
+  def test_invalid_directives
+    assert_not_parses("identifier", '@<err> import "foo";')
+    assert_not_parses("identifier", '@<err>12 "foo";')
+  end
+
   private
 
   def assert_valid_string(ident)
@@ -455,6 +462,29 @@ SCSS
 
   def assert_parses(scss)
     assert_equal scss.rstrip, render(scss, munge_filename).rstrip
+  end
+
+  def assert_not_parses(expected, scss)
+    raise "Template must include <err> where an error is expected" unless scss.include?("<err>")
+
+    after, was = scss.split("<err>")
+    line = after.count("\n") + 1
+
+    after.gsub!(/\s*\n\s*$/, '')
+    after.gsub!(/.*\n/, '')
+    after = "..." + after[-15..-1] if after.size > 18
+
+    was.gsub!(/^\s*\n\s*/, '')
+    was.gsub!(/\n.*/, '')
+    was = was[0...15] + "..." if was.size > 18
+
+    to_render = scss.sub("<err>", "")
+    render(to_render, munge_filename)
+    assert(false, "Expected syntax error for:\n#{to_render}\n")
+  rescue Sass::SyntaxError => err
+    assert_equal("Invalid CSS after \"#{after}\": expected #{expected}, was \"#{was}\"",
+      err.message)
+    assert_equal line, err.sass_line
   end
 
   def render(scss, options = {})
