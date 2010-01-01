f028f4c43c77df1967294b83a818003ed3a91492
Nathan Weizenbaum <nex342@gmail.com>
2009-12-31 20:49:35 -0800

    [Sass] [SCSS] Document sass/scss/script_parser.rb.

--- lib/sass/scss/script_parser.rb
+++ lib/sass/scss/script_parser.rb
@@ -1,8 +1,14 @@
 module Sass
   module SCSS
+    # A subclass of {Sass::Script::Parser}
+    # that can be used as a subcomponent of {SCSS::Parser}.
+    # In particular, the parser doesn't raise an error
+    # when there's more content in the lexer once lexing is done.
     class ScriptParser < Sass::Script::Parser
       private
 
+      # Instead of raising an error when the parser is done,
+      # rewind the StringScanner so that it hasn't consumed the final token.
       def assert_done
         @lexer.unpeek!
       end
