e3ca1ad4206b3681c3f0dd037baf8c977c1c894d
Nathan Weizenbaum <nex342@gmail.com>
2010-02-08 17:15:01 -0800

    [Haml] Properly namespace the XSS overrides of some actionview stuff.

--- lib/haml/helpers/xss_mods.rb
+++ lib/haml/helpers/xss_mods.rb
@@ -98,27 +98,27 @@ end
 module ActionView
   module Helpers
     module TextHelper
-      def concat_with_haml(string)
+      def concat_with_haml_xss(string)
         if is_haml?
           haml_buffer.buffer.concat(haml_xss_html_escape(string))
         else
-          concat_without_haml(string)
+          concat_without_haml_xss(string)
         end
       end
-      alias_method :concat_without_haml, :concat
-      alias_method :concat, :concat_with_haml
+      alias_method :concat_without_haml_xss, :concat
+      alias_method :concat, :concat_with_haml_xss
 
       # safe_concat was introduced in Rails 3.0
       if Haml::Util.has?(:instance_method, self, :safe_concat)
-        def safe_concat_with_haml(string)
+        def safe_concat_with_haml_xss(string)
           if is_haml?
             haml_buffer.buffer.concat(string)
           else
-            safe_concat_without_haml(string)
+            safe_concat_without_haml_xss(string)
           end
         end
-        alias_method :safe_concat_without_haml, :safe_concat
-        alias_method :safe_concat, :safe_concat_with_haml
+        alias_method :safe_concat_without_haml_xss, :safe_concat
+        alias_method :safe_concat, :safe_concat_with_haml_xss
       end
     end
   end
