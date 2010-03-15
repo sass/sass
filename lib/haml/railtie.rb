cb98a114f8bb316faa38b42b228b835f50a91271
Nathan Weizenbaum <nex342@gmail.com>
2010-03-14 17:15:33 -0700

    Add a note about when to remove the railtie.

--- lib/haml/railtie.rb
+++ lib/haml/railtie.rb
@@ -1,3 +1,8 @@
+# This file is here to integrate with Rails 3,
+# since there's no better way to do so as of 14 March 2010.
+# Yehuda promises there will be soon,
+# and once there is we should switch to that.
+
 if defined?(Rails::Railtie)
   module Haml
     class Railtie < Rails::Railtie
