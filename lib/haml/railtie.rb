6669e8ddc7405d4127df96ac78946b71045e6b97
Nathan Weizenbaum <nex342@gmail.com>
2010-05-14 00:33:15 -0700

    Get rid of an outdated comment.

--- lib/haml/railtie.rb
+++ lib/haml/railtie.rb
@@ -1,8 +1,3 @@
-# This file is here to integrate with Rails 3,
-# since there's no better way to do so as of 14 March 2010.
-# Yehuda promises there will be soon,
-# and once there is we should switch to that.
-
 # Rails 3.0.0.beta.2+
 if defined?(ActiveSupport) && Haml::Util.has?(:public_method, ActiveSupport, :on_load)
   require 'haml/template/options'
