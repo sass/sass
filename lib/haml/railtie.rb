ebdb0753ae902ae0a10aeebde66543103f2caabd
Nathan Weizenbaum <nex342@gmail.com>
2010-06-10 14:09:18 -0700

    Fully remove support for Rails 3b3 and earlier.

--- lib/haml/railtie.rb
+++ lib/haml/railtie.rb
@@ -1,3 +1,10 @@
+if Haml::Util.ap_geq_3? && !Haml::Util.ap_geq?("3.0.0.beta4")
+  raise <<ERROR
+Haml and Sass no longer support Rails 3 versions before beta 4.
+  Please upgrade to Rails 3.0.0.beta4 or later.
+ERROR
+end
+
 # Rails 3.0.0.beta.2+
 if defined?(ActiveSupport) && Haml::Util.has?(:public_method, ActiveSupport, :on_load)
   require 'haml/template/options'
