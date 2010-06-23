39176452d6d467abf3cf714b4383f7a9c208b601
Nathan Weizenbaum <nex342@gmail.com>
2010-06-23 14:27:33 -0700

    Bump VERSION to 3.0.13.

--- doc-src/SASS_CHANGELOG.md
+++ doc-src/SASS_CHANGELOG.md
@@ -3,7 +3,9 @@
 * Table of contents
 {:toc}
 
-## 3.0.13 (Unreleased)
+## 3.0.13
+
+[Tagged on GitHub](http://github.com/nex3/haml/commit/3.0.12).
 
 ## CSS `@import` Directives
 
--- extra/sass-mode.el
+++ extra/sass-mode.el
@@ -4,11 +4,11 @@
 
 ;; Author: Nathan Weizenbaum
 ;; URL: http://github.com/nex3/haml/tree/master
-;; Version: 3.0.10
+;; Version: 3.0.13
 ;; Created: 2007-03-15
 ;; By: Nathan Weizenbaum
 ;; Keywords: markup, language, css
-;; Package-Requires: ((haml-mode "3.0.10"))
+;; Package-Requires: ((haml-mode "3.0.13"))
 
 ;;; Commentary:
 
