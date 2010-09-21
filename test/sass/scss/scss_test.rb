aab0567f6f7760061fc43caa7a2e8931153f0083
Chris Eppstein <chris@eppsteins.net>
2010-09-21 07:55:38 -0700

    Fix bug in compressed output of SCSS selectors with newlines. Closes GH-275.

--- test/sass/scss/scss_test.rb
+++ test/sass/scss/scss_test.rb
@@ -1071,4 +1071,18 @@ $bar : "#foo";
 ul li\#{$bar} a span.label { foo: bar; }
 SCSS
   end
+
+  def test_bug_275
+    assert_equal <<CSS, render(<<SCSS, :style=>:compressed)
+z a,z b{display:block}
+CSS
+a,
+b {
+  z & {
+    display: block;
+  }
+}
+SCSS
+
+  end
 end
