f314f35c17ae420b4ff580b52b44e36346da4bee
Nathan Weizenbaum <nex342@gmail.com>
2010-10-19 01:03:03 -0700

    Make the documentation for Sass.compile_file a little clearer.

--- lib/sass.rb
+++ lib/sass.rb
@@ -43,9 +43,13 @@ module Sass
   # @raise [ArgumentError] if the document uses an unknown encoding with `@charset`
   #
   # @overload compile_file(filename, options = {})
+  #   Return the compiled CSS rather than writing it to a file.
+  #
   #   @return [String] The compiled CSS.
   #
   # @overload compile_file(filename, css_filename, options = {})
+  #   Write the compiled CSS to a file.
+  #
   #   @param css_filename [String] The location to which to write the compiled CSS.
   def self.compile_file(filename, *args)
     options = args.last.is_a?(Hash) ? args.pop : {}
