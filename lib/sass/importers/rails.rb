5309c5018413f6c194d3223fc71d401126080018
Nathan Weizenbaum <nex342@gmail.com>
2010-11-18 21:39:43 -0800

    Fix a syntax error in the Rails importer.

--- lib/sass/importers/rails.rb
+++ lib/sass/importers/rails.rb
@@ -55,9 +55,9 @@ module Sass
       end
 
       def find_template(uri, prefix, partial)
-        return @lookup_context
-          .find_all(uri, prefix, partial)
-          .find {|t| t.handler.is_a?(Sass::Plugin::TemplateHandler)}
+        return @lookup_context.
+          find_all(uri, prefix, partial).
+          find {|t| t.handler.is_a?(Sass::Plugin::TemplateHandler)}
       end
 
       def prepare_template(template, options)
