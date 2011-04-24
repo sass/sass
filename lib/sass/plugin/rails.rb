1f56925df17ec8196fd6687b9f16f2da2b6b2a2d
Nathan Weizenbaum <nex342@gmail.com>
2011-04-24 15:30:40 -0700

    Nuke all Rails 3.1 support.
    The new plan is to have Rails take care of any and all integration and
    compilation.

--- lib/sass/plugin/rails.rb
+++ lib/sass/plugin/rails.rb
@@ -11,18 +11,11 @@ unless defined?(Sass::RAILS_LOADED)
         :cache_location    => Sass::Util.rails_root + '/tmp/sass-cache'
       }
 
-      if Sass::Util.ap_geq?('3.1.0.beta')
-        require 'sass/importers/rails'
-        require 'sass/cache_stores/active_support'
-        opts.merge!(:load_paths => [Sass::Importers::Rails.new])
-        opts.merge!(:cache_store => Sass::CacheStores::ActiveSupport.new(Rails.cache)) if Rails.cache
-      else
-        opts.merge!(
-          :always_update      => false,
-          :template_location => Sass::Util.rails_root + '/public/stylesheets/sass',
-          :css_location      => Sass::Util.rails_root + '/public/stylesheets',
-          :always_check      => Sass::Util.rails_env == "development")
-      end
+      opts.merge!(
+        :always_update     => false,
+        :template_location => Sass::Util.rails_root + '/public/stylesheets/sass',
+        :css_location      => Sass::Util.rails_root + '/public/stylesheets',
+        :always_check      => Sass::Util.rails_env == "development")
 
       @default_options = opts.freeze
     end
@@ -30,76 +23,9 @@ unless defined?(Sass::RAILS_LOADED)
 
   Sass::Plugin.options.reverse_merge!(Sass::Plugin.default_options)
 
-  # Disable this for now, until we figure out how to get Rails
-  # to pass in the view.
-  if Sass::Util.ap_geq?('3.1.0.beta')
-    class Sass::Plugin::TemplateHandler
-      attr_reader :syntax
-
-      def initialize(syntax)
-        @syntax = syntax
-      end
-
-      def handles_encoding?; true; end
-
-      def call(template, view)
-        engine = Sass::Engine.new(template.source,
-          Sass::Plugin.engine_options.merge(
-            :syntax => @syntax,
-            :filename => template.virtual_path,
-            :_rails_lookup_context => view.lookup_context,
-            :importer => Sass::Importers::Rails.new))
-
-        template.data[:sass_importers] = engine.dependencies.map do |e|
-          [e.options[:filename], e.options[:importer]]
-        end
-
-        stylesheet =
-          begin
-            engine.render
-          rescue Sass::SyntaxError => e
-            Sass::Plugin::TemplateHandler.munge_exception e, view.lookup_context
-            Sass::SyntaxError.exception_to_css(e, Sass::Plugin.engine_options)
-          end
-
-        <<RUBY
-begin
-  if Sass::Plugin::TemplateHandler.dependencies_changed?(
-      @_template.data[:sass_importers], #{Time.now.to_i}, lookup_context)
-    @_template.expire!
-    @_template.rerender(self)
-  else
-    #{stylesheet.inspect}
-  end
-rescue Sass::SyntaxError => e
-  Sass::Plugin::TemplateHandler.munge_exception e, lookup_context
-  Sass::SyntaxError.exception_to_css(e, Sass::Plugin.engine_options)
-end
-RUBY
-      end
-
-      def self.dependencies_changed?(deps, since, lookup_context)
-        opts = Sass::Plugin.engine_options.merge(:_rails_lookup_context => lookup_context)
-        deps.any? do |d, i|
-          return true unless time = i.mtime(d, opts)
-          time.to_i > since
-        end
-      end
-
-      def self.munge_exception(e, lookup_context)
-        importer = Sass::Importers::Rails.new
-        opts = Sass::Plugin.engine_options.merge(:_rails_lookup_context => lookup_context)
-        e.sass_backtrace.each do |bt|
-          next unless engine = importer.find(bt[:filename], opts)
-          bt[:filename] = engine.options[:_rails_filename]
-        end
-      end
-    end
-
-    ActionView::Template.register_template_handler(:sass, Sass::Plugin::TemplateHandler.new(:sass))
-    ActionView::Template.register_template_handler(:scss, Sass::Plugin::TemplateHandler.new(:scss))
-  elsif defined?(ActionController::Metal)
-    # Rails >= 3.0
+  # Rails 3.1 loads and handles Sass all on its own
+  if defined?(ActionController::Metal)
+    # 3.1 > Rails >= 3.0
     require 'sass/plugin/rack'
     Rails.configuration.middleware.use(Sass::Plugin::Rack)
   elsif defined?(ActionController::Dispatcher) &&
--- lib/sass/railtie.rb
+++ lib/sass/railtie.rb
@@ -1,5 +1,6 @@
-# Rails 3.0.0.beta.2+
-if defined?(ActiveSupport) && Sass::Util.has?(:public_method, ActiveSupport, :on_load)
+# Rails 3.0.0.beta.2+, < 3.1
+if defined?(ActiveSupport) && Sass::Util.has?(:public_method, ActiveSupport, :on_load) &&
+    !Sass::Util.ap_geq?('3.1.0.beta')
   require 'sass/plugin/configuration'
   ActiveSupport.on_load(:before_initialize) do
     require 'sass'
