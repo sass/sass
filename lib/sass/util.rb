382aa5db1cdcee6641209018b046dc6ce348aabe
Nathan Weizenbaum <nweiz@google.com>
2013-02-01 17:18:32 -0800

    Fix error-character quoting for encoding errors on Rubinius 2 in 1.9 mode.

--- lib/sass/util.rb
+++ lib/sass/util.rb
@@ -275,6 +275,8 @@ module Sass
     # @param [Encoding::UndefinedConversionError]
     # @return [String]
     def undefined_conversion_error_char(e)
+      # Rubinius (as of 2.0.0.rc1) pre-quotes the error character.
+      return e.error_char if rbx?
       # JRuby (as of 1.7.2) doesn't have an error_char field on
       # Encoding::UndefinedConversionError.
       return e.error_char.dump unless jruby?
