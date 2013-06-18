b25834f30a7567a7bbdfbbbbb43681cd960bccb2
Chris Eppstein <chris@eppsteins.net>
2013-06-18 14:16:49 -0700

    Documentation and code-style cleanup.

--- lib/sass/script/value/bool.rb
+++ lib/sass/script/value/bool.rb
@@ -2,14 +2,20 @@ module Sass::Script::Value
   # A SassScript object representing a boolean (true or false) value.
   class Bool < Base
 
-    TRUE  = self.new(true)
-    FALSE = self.new(false)
+    # The true value in SassScript.
+    #
+    # This is assigned before new is overridden below so that we use the default implementation.
+    TRUE  = new(true)
+
+    # The false value in SassScript.
+    #
+    # This is assigned before new is overridden below so that we use the default implementation.
+    FALSE = new(false)
 
     # We override object creation so that users of the core API
     # will not need to know that booleans are specific constants.
     #
     # @param value A ruby value that will be tested for truthiness.
-    #
     # @return [Bool] TRUE if value is truthy, FALSE if value is falsey
     def self.new(value)
       value ? TRUE : FALSE
@@ -27,5 +33,4 @@ module Sass::Script::Value
     end
     alias_method :to_sass, :to_s
   end
-
 end
