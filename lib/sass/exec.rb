ba1a277072f6970a5679bd8ba629ed288c0e6e2f
Collin Klopfenstein <collin.klopfenstein@gmail.com>
2013-11-04 12:34:57 -0600

    Add --bell and --bwatch command line options
    Add --bell and --bwatch (shortcut for `--bell --watch`) command line
    options.  --bell rings a system bell when a file is compiled.

--- lib/sass/exec.rb
+++ lib/sass/exec.rb
@@ -124,6 +124,7 @@ module Sass
       #   Can be `:red`, `:green`, or `:yellow`.
       def puts_action(name, color, arg)
         return if @options[:for_engine][:quiet]
+        printf "\a" if @options[:bell]
         printf color(color, "%11s %s\n"), name, arg
         STDOUT.flush
       end
@@ -233,6 +234,16 @@ END
                            "  #{@default_syntax} --watch input-dir:output-dir") do
           @options[:watch] = true
         end
+        opts.on('--bell', 'Output bell when compilation is finished.') do
+          @options[:bell] = true
+        end
+        opts.on('--bwatch', 'Watches file or directories for changes and outputs a bell after compilation is finished.',
+                            'The location of the generated CSS can be set using a colon:',
+                            "  #{@default_syntax} --watch input.#{@default_syntax}:output.css",
+                            "  #{@default_syntax} --watch input-dir:output-dir") do
+          @options[:watch] = true
+          @options[:bell] = true
+		end
         opts.on('--update', 'Compile files or directories to CSS.',
                             'Locations are set like --watch.') do
           @options[:update] = true
