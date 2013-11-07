481ba30dd21088a26e0b9d65f33b728cc40d22ec
Chris Eppstein <chris@eppsteins.net>
2013-11-06 16:19:18 -0800

    Storing node names against the class results in a marginal speedup.

--- lib/sass/tree/visitors/base.rb
+++ lib/sass/tree/visitors/base.rb
@@ -61,7 +61,7 @@ module Sass::Tree::Visitors
     # @return [String] The name.
     def self.node_name(node)
       @node_names ||= {}
-      @node_names[node.class.name] ||= node.class.name.gsub(NODE_NAME_RE, '\\1').downcase
+      @node_names[node.class] ||= node.class.name.gsub(NODE_NAME_RE, '\\1').downcase
     end
 
     # `yield`s, then runs the visitor on the `@else` clause if the node has one.
