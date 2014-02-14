69308c22b8518d65dee0db36241df423284e85f8
Nathan Weizenbaum <nweiz@google.com>
2014-02-14 13:22:09 -0800

    Install the old version of listen for Ruby 1.9.2.

--- ext/mkrf_conf.rb
+++ ext/mkrf_conf.rb
@@ -1,20 +1,24 @@
 require 'rubygems'
 require 'rubygems/command.rb'
-require 'rubygems/dependency_installer.rb' 
+require 'rubygems/dependency_installer.rb'
+
+require '../lib/sass'
 
 # This script installs the correct version of listen. Listen versions
 # beyond 1.1 don't support Ruby 1.8, any RubyGems isn't clever enough
 # to install the most recent version that works, so we have to do it
 # manually.
 
+puts "Ensuring you have the right version of listen installed."
+
 Gem::Command.build_args = ARGV
 inst = Gem::DependencyInstaller.new
-if RUBY_VERSION < "1.9"
-  puts "Installing listen ~> 1.1.0"
-  inst.install "listen", "~> 1.1.0"
-else
+if Sass::Util.version_geq(RUBY_VERSION, "1.9.3")
   puts "Installing listen >= 1.1.0, < 2.5"
   inst.install "listen", Gem::Requirement.new(">= 1.1.0", "< 2.5")
+else
+  puts "Installing listen ~> 1.1.0"
+  inst.install "listen", "~> 1.1.0"
 end
 
 # Create a dummy rakefile to indicate success.
