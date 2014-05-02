7ab4f552daae6d8789172ee1d246746e0071c28d
Nathan Weizenbaum <nweiz@google.com>
2014-05-02 14:36:53 -0700

    Fix a RuboCop error.

--- lib/sass/scss/parser.rb
+++ lib/sass/scss/parser.rb
@@ -1235,7 +1235,7 @@ MESSAGE
 
         unless name
           # Display basic regexps as plain old strings
-          source = rx.source.gsub(%r{\\/}, '/')
+          source = rx.source.gsub(/\\\//, '/')
           string = rx.source.gsub(/\\(.)/, '\1')
           name = source == Regexp.escape(string) ? string.inspect : rx.inspect
         end
