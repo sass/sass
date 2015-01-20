4c9f5891b48d234e8dd24bd55a3158a31b204e3e
Nathan Weizenbaum <nex342@gmail.com>
2015-01-20 13:25:44 -0800

    Fix a scss_test test.

--- lib/sass/ruby2ruby.rb
+++ lib/sass/ruby2ruby.rb
@@ -7,5 +7,41 @@ module Sass
     def process_comment(exp)
       exp.shift.to_s
     end
+
+    def process_iter(exp) # :nodoc:
+      iter = process exp.shift
+      args = exp.shift
+      body = exp.empty? ? nil : process(exp.shift)
+
+      args = case args
+             when 0 then
+               " ||"
+             else
+               a = process(args)[1..-2]
+               a = " |#{a}|" unless a.empty?
+               a
+             end
+
+      b, e = if iter == "END" then
+               [ "{", "}" ]
+             else
+               [ "do", "end" ]
+             end
+
+      iter.sub!(/\(\)$/, '')
+
+      # Avoid a one-line lambda in case it contains a comment.
+
+      result = []
+      result << "#{iter} #{b}"
+      result << args
+      result << "\n"
+      if body then
+        result << indent(body.strip)
+        result << "\n"
+      end
+      result << e
+      result.join
+    end
   end
 end
