d533f06a6ff7888e3f664dcd59698f25b42fd1b0
Nathan Weizenbaum <nex342@gmail.com>
2015-01-30 17:03:16 -0800

    Rip out unused Environment stuff.

--- lib/sass/environment.rb
+++ lib/sass/environment.rb
@@ -2,7 +2,7 @@ require 'set'
 
 module Sass
   # The abstract base class for lexical environments for SassScript.
-  class BaseEnvironment
+  class Environment
     class << self
       # Note: when updating this,
       # update sass/yard/inherited_hash.rb as well.
@@ -62,43 +62,20 @@ module Sass
       end
     end
 
-    # The options passed to the Sass Engine.
-    attr_reader :options
-
-    attr_writer :caller
-    attr_writer :content
-    attr_writer :selector
-
-    # variable
-    # Script::Value
-    inherited_hash_reader :var
-
-    # mixin
-    # Sass::Callable
-    inherited_hash_reader :mixin
-
-    # function
-    # Sass::Callable
-    inherited_hash_reader :function
-
     inherited_hash_reader :fn
     inherited_hash_writer :fn
 
     inherited_hash_reader :mx
     inherited_hash_writer :mx
 
+    inherited_hash_reader :var
     inherited_hash_writer :var
 
     # @param options [{Symbol => Object}] The options hash. See
     #   {file:SASS_REFERENCE.md#sass_options the Sass options documentation}.
     # @param parent [Environment] See \{#parent}
-    def initialize(parent = nil, options = nil, mapper = nil, fn_signatures = nil, mx_signatures = nil)
-      @fn_signatures = fn_signatures
-      @mx_signatures = mx_signatures
+    def initialize(parent = nil)
       @parent = parent
-      @options = options || (parent && parent.options) || {}
-      @stack = Sass::Stack.new if @parent.nil?
-      @mapper = mapper
       @ident_count = 0
       @idents = {}
     end
@@ -166,262 +143,11 @@ module Sass
       global?
     end
 
-    # The environment of the caller of this environment's mixin or function.
-    # @return {Environment?}
-    def caller
-      @caller || (@parent && @parent.caller)
-    end
-
-    def mapper
-      @mapper || (@parent && @parent.mapper)
-    end
-
-    def fn_signatures
-      @fn_signatures || (@parent && @parent.fn_signatures)
-    end
-
-    def mx_signatures
-      @mx_signatures || (@parent && @parent.mx_signatures)
-    end
-
-    # The content passed to this environment. This is naturally only set
-    # for mixin body environments with content passed in.
-    #
-    # @return {[Array<Sass::Tree::Node>, Environment]?} The content nodes and
-    #   the lexical environment of the content block.
-    def content
-      @content || (@parent && @parent.content)
-    end
-
-    # The selector for the current CSS rule, or nil if there is no
-    # current CSS rule.
-    #
-    # @return [Selector::CommaSequence?] The current selector, with any
-    #   nesting fully resolved.
-    def selector
-      @selector || (@caller && @caller.selector) || (@parent && @parent.selector)
-    end
-
     # The top-level Environment object.
     #
     # @return [Environment]
     def global_env
       @global_env ||= global? ? self : @parent.global_env
-    end
-
-    # The import/mixin stack.
-    #
-    # @return [Sass::Stack]
-    def stack
-      mapper.stack_for Kernel.caller
-    end
-
-    def run_mixin(context, name, splat)
-      callable = mx_signatures[name]
-      return run_callable(context, callable, splat) if callable
-      raise Sass::SyntaxError.new("Undefined mixin '#{name}'.")
-    end
-
-    def run_callable(context, callable, splat)
-      # TODO: test all calling convention stuff twice, once for static
-      # and once for dynamic calls.
-      desc = "#{callable.type.capitalize} #{callable.name}"
-      downcase_desc = "#{callable.type} #{callable.name}"
-
-      # All keywords are contained in splat.keywords for consistency,
-      # even if there were no splats passed in.
-      keywords = splat.keywords_safe
-
-      begin
-        unless keywords.empty?
-          unknown_args = Sass::Util.array_minus(keywords.keys,
-            callable.args.map {|var| var && var.first.underscored_name}.compact)
-          if callable.splat && unknown_args.include?(callable.splat.underscored_name)
-            raise Sass::SyntaxError.new("Argument $#{callable.splat.name} of #{downcase_desc} " +
-                                        "cannot be used as a named argument.")
-          elsif unknown_args.any?
-            description = unknown_args.length > 1 ? 'the following arguments:' : 'an argument named'
-            raise Sass::SyntaxError.new("#{desc} doesn't have #{description} " +
-                                        "#{unknown_args.map {|name| "$#{name}"}.join ', '}.")
-          end
-        end
-      rescue Sass::SyntaxError => keyword_exception
-      end
-
-      # If there's no splat, raise the keyword exception immediately. The actual
-      # raising happens in the ensure clause at the end of this function.
-      return if keyword_exception && !callable.splat
-
-      args = splat.to_a
-      splat_sep = splat.separator
-
-      if args.size > callable.args.size && !callable.splat
-        extra_args_because_of_splat = splat && args.size - splat.to_a.size <= callable.args.size
-
-        takes = callable.args.size
-        passed = args.size
-        message = "#{desc} takes #{takes} argument#{'s' unless takes == 1} " +
-          "but #{passed} #{passed == 1 ? 'was' : 'were'} passed."
-        raise Sass::SyntaxError.new(message) unless extra_args_because_of_splat
-        # TODO: when the deprecation period is over, make this an error.
-        Sass::Util.sass_warn("WARNING: #{message}\n" +
-          stack.to_s.gsub(/^/m, " " * 8) + "\n" +
-          "This will be an error in future versions of Sass.")
-      end
-
-      ruby_args = callable.args.zip(args[0...callable.args.length]).map do |(var, default), value|
-        # var will be nil for Ruby functions without declared signatures.
-        next value if var.nil?
-
-        if value && keywords.has_key?(var.name)
-          raise Sass::SyntaxError.new("#{desc} was passed argument $#{var.name} " +
-                                      "both by position and by name.")
-        end
-
-        value ||= keywords.delete(var.name)
-        raise Sass::SyntaxError.new("#{desc} is missing argument #{var.inspect}.") unless value
-        value
-      end
-
-      if callable.splat
-        rest = args[callable.args.length..-1] || []
-        arg_list = Sass::Script::Value::ArgList.new(rest, keywords, splat_sep)
-        ruby_args << arg_list
-      end
-
-      callable.run(context, ruby_args)
-    rescue StandardError => e
-    ensure
-      # If there's a keyword exception, we don't want to throw it immediately,
-      # because the invalid keywords may be part of a glob argument that should be
-      # passed on to another function. So we only raise it if we reach the end of
-      # this function *and* the keywords attached to the argument list glob object
-      # haven't been accessed.
-      #
-      # The keyword exception takes precedence over any Sass errors, but not over
-      # non-Sass exceptions.
-      if keyword_exception &&
-          !(arg_list && arg_list.keywords_accessed) &&
-          (e.nil? || e.is_a?(Sass::SyntaxError))
-        raise keyword_exception
-      elsif e
-        raise e
-      end
-    end
-
-    def run_ruby_function(context, name, ruby_name, splat)
-      context.send(ruby_name, *construct_ruby_args(name, ruby_name, splat))
-    rescue ArgumentError => e
-      Sass::Script::Helpers.reformat_argument_error(ruby_name, name, e)
-    end
-
-    def construct_ruby_args(name, ruby_name, splat)
-      args = splat.value
-      keywords = splat.keywords_safe
-
-      unless (signature = Sass::Script::Functions.signature(
-               ruby_name.to_sym, args.size, keywords.size))
-        return splat.to_a if keywords.empty?
-        raise Sass::SyntaxError.new("Function #{name} doesn't support keyword arguments.")
-      end
-
-      # If the user passes more non-keyword args than the function expects,
-      # but it does expect keyword args, Ruby's arg handling won't raise an error.
-      # Since we don't want to make functions think about this,
-      # we'll handle it for them here.
-      if signature.var_kwargs && !signature.var_args && args.size > signature.args.size
-        raise Sass::SyntaxError.new(
-          "#{args[signature.args.size].inspect} is not a keyword argument for `#{name}'.")
-      elsif keywords.empty?
-        return args
-      end
-
-      argnames = signature.args[args.size..-1] || []
-      deprecated_argnames = (signature.deprecated && signature.deprecated[args.size..-1]) || []
-      args = args + argnames.zip(deprecated_argnames).map do |(argname, deprecated_argname)|
-        if keywords.has_key?(argname)
-          keywords.delete(argname)
-        elsif deprecated_argname && keywords.has_key?(deprecated_argname)
-          deprecated_argname = keywords.denormalize(deprecated_argname)
-          Sass::Util.sass_warn("DEPRECATION WARNING: The `$#{deprecated_argname}' argument for " +
-            "`#{@name}()' has been renamed to `$#{argname}'.")
-          keywords.delete(deprecated_argname)
-        else
-          raise Sass::SyntaxError.new("Function #{name} requires an argument named $#{argname}.")
-        end
-      end
-
-      if keywords.size > 0
-        if signature.var_kwargs
-          # Don't pass a NormalizedMap to a Ruby function.
-          args << keywords.to_hash
-        else
-          argname = keywords.keys.sort.first
-          if signature.args.include?(argname)
-            raise Sass::SyntaxError.new(
-              "Function #{name} was passed argument $#{argname} both by position and by name")
-          else
-            raise Sass::SyntaxError.new(
-              "Function #{name} doesn't have an argument named $#{argname}")
-          end
-        end
-      end
-
-      args
-    end
-  end
-
-  # The lexical environment for SassScript.
-  # This keeps track of variable, mixin, and function definitions.
-  #
-  # A new environment is created for each level of Sass nesting.
-  # This allows variables to be lexically scoped.
-  # The new environment refers to the environment in the upper scope,
-  # so it has access to variables defined in enclosing scopes,
-  # but new variables are defined locally.
-  #
-  # Environment also keeps track of the {Engine} options
-  # so that they can be made available to {Sass::Script::Functions}.
-  class Environment < BaseEnvironment
-    # The enclosing environment,
-    # or nil if this is the global environment.
-    #
-    # @return [Environment]
-    attr_reader :parent
-
-    # variable
-    # Script::Value
-    inherited_hash_writer :var
-
-    # mixin
-    # Sass::Callable
-    inherited_hash_writer :mixin
-
-    # function
-    # Sass::Callable
-    inherited_hash_writer :function
-  end
-
-  # A read-only wrapper for a lexical environment for SassScript.
-  class ReadOnlyEnvironment < BaseEnvironment
-    # The read-only environment of the caller of this environment's mixin or function.
-    #
-    # @see BaseEnvironment#caller
-    # @return {ReadOnlyEnvironment}
-    def caller
-      return @caller if @caller
-      env = super
-      @caller ||= env.is_a?(ReadOnlyEnvironment) ? env : ReadOnlyEnvironment.new(env, env.options)
-    end
-
-    # The read-only content passed to this environment.
-    #
-    # @see BaseEnvironment#content
-    # @return {ReadOnlyEnvironment}
-    def content
-      return @content if @content
-      env = super
-      @content ||= env.is_a?(ReadOnlyEnvironment) ? env : ReadOnlyEnvironment.new(env, env.options)
     end
   end
 
