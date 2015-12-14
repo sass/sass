sass/sass

commit 0a8000947e1ac6540b285652d7a6963bc91aba76

Fix missing semicolons. Closes #1806.

lib/sass/tree/visitors/to_css.rb
@@ -51,6 +51,10 @@ class Sass::Tree::Visitors::ToCss < Sass::Tree::Visitors::Base
     @source_mapping.add(source_range, target_range)
   end
 
+  def ends_with?(str)
+    @result.end_with?(str)
+  end
+
   # Move the output cursor back `chars` characters.
   def erase!(chars)
     return if chars == 0
@@ -124,6 +128,9 @@ class Sass::Tree::Visitors::ToCss < Sass::Tree::Visitors::Base
       end
     end
     rstrip!
+    if node.style == :compressed && ends_with?(";")
+      erase! 1
+    end
     return "" if @result.empty?
 
     output "\n"
@@ -171,10 +178,10 @@ class Sass::Tree::Visitors::ToCss < Sass::Tree::Visitors::Base
     if !node.has_children || node.children.empty?
       output(tab_str)
       for_node(node) {output(node.resolved_value)}
-      if node.style == :compressed
-        output("{}") if node.has_children
-      else
-        output(node.has_children ? " {}" : ";")
+      if node.has_children
+        output("#{' ' unless node.style == :compressed}{}")
+      elsif node.children.empty?
+        output(";")
       end
       return
     end
@@ -213,7 +220,9 @@ class Sass::Tree::Visitors::ToCss < Sass::Tree::Visitors::Base
         had_children = child.has_children
         first = false
       elsif node.style == :compressed
-        output(had_children ? "" : ";")
+        unless had_children
+          output(";") unless ends_with?(";")
+        end
         with_tabs(0) {visit(child)}
         had_children = child.has_children
       else
@@ -222,6 +231,9 @@ class Sass::Tree::Visitors::ToCss < Sass::Tree::Visitors::Base
       end
     end
     rstrip!
+    if node.style == :compressed && ends_with?(";")
+      erase! 1
+    end
     if node.style == :expanded
       output("\n#{tab_str}")
     elsif node.style != :compressed
@@ -342,9 +354,17 @@ class Sass::Tree::Visitors::ToCss < Sass::Tree::Visitors::Base
 
       with_tabs(tabs) do
         node.children.each_with_index do |child, i|
-          output(separator) if i > 0
+          if i > 0
+            if separator.start_with?(";") && ends_with?(";")
+              erase! 1
+            end
+            output(separator)
+          end
           visit(child)
         end
+      end
+      if node.style == :compressed && ends_with?(";")
+        erase! 1
       end
 
       output(end_props)

commit 40a0d14d4af02d8a3a5669642d7ccc01c7fd7684

Don't strip escaped semicolons from compressed output.
Fixes a regression introduced in
0a8000947e1ac6540b285652d7a6963bc91aba76 and released in 3.4.18.

Closes #1932.

lib/sass/tree/visitors/to_css.rb
@@ -51,8 +51,8 @@ class Sass::Tree::Visitors::ToCss < Sass::Tree::Visitors::Base
     @source_mapping.add(source_range, target_range)
   end
 
-  def ends_with?(str)
-    @result.end_with?(str)
+  def trailing_semicolon?
+   @result.end_with?(";") && !@result.end_with?('\;')
   end
 
   # Move the output cursor back `chars` characters.
@@ -128,7 +128,7 @@ class Sass::Tree::Visitors::ToCss < Sass::Tree::Visitors::Base
       end
     end
     rstrip!
-    if node.style == :compressed && ends_with?(";")
+    if node.style == :compressed && trailing_semicolon?
       erase! 1
     end
     return "" if @result.empty?
@@ -222,7 +222,7 @@ class Sass::Tree::Visitors::ToCss < Sass::Tree::Visitors::Base
         first = false
       elsif node.style == :compressed
         unless had_children
-          output(";") unless ends_with?(";")
+          output(";") unless trailing_semicolon?
         end
         with_tabs(0) {visit(child)}
         had_children = child.has_children
@@ -232,7 +232,7 @@ class Sass::Tree::Visitors::ToCss < Sass::Tree::Visitors::Base
       end
     end
     rstrip!
-    if node.style == :compressed && ends_with?(";")
+    if node.style == :compressed && trailing_semicolon?
       erase! 1
     end
     if node.style == :expanded
@@ -359,7 +359,7 @@ class Sass::Tree::Visitors::ToCss < Sass::Tree::Visitors::Base
       with_tabs(tabs) do
         node.children.each_with_index do |child, i|
           if i > 0
-            if separator.start_with?(";") && ends_with?(";")
+            if separator.start_with?(";") && trailing_semicolon?
               erase! 1
             end
             output(separator)
@@ -367,7 +367,7 @@ class Sass::Tree::Visitors::ToCss < Sass::Tree::Visitors::Base
           visit(child)
         end
       end
-      if node.style == :compressed && ends_with?(";")
+      if node.style == :compressed && trailing_semicolon?
         erase! 1
       end
 

test/sass/engine_test.rb
@@ -3322,6 +3322,16 @@ CSS
 SASS
   end
 
+  def test_escaped_semicolons_are_not_compressed
+    assert_equal(<<'CSS', render(<<'SASS', :syntax => :scss, :style => :compressed))
+div{color:#f00000\9\0\;}
+CSS
+div {
+ color: #f00000\9\0\;
+}
+SASS
+  end
+
   def test_compressed_output_of_nth_selectors
     assert_equal(<<CSS, render(<<SASS, :syntax => :scss, :style => :compressed))
 :nth-of-type(2n-1),:nth-child(2n-1),:nth(2n-1),:nth-of-type(2n-1),:nth-of-type(2n-1){color:red}:nth-of-type(2n+1),:nth-child(2n+1),:nth(2n+1),:nth-of-type(2n+1),:nth-of-type(2n+1){color:red}