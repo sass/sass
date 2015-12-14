sass/sass

commit 6e351f0c44f50ed98e79ca68ddac9af80e65bcca

Better compression for `:nth` pseudoselectors with subtraction.
Closes #1874.

lib/sass/tree/visitors/to_css.rb
@@ -297,7 +297,7 @@ class Sass::Tree::Visitors::ToCss < Sass::Tree::Visitors::Base
         rule_part = seq.to_s
         if node.style == :compressed
           rule_part.gsub!(/([^,])\s*\n\s*/m, '\1 ')
-          rule_part.gsub!(/\s*([,+>])\s*/m, '\1')
+          rule_part.gsub!(/\s*([-,+>])\s*/m, '\1')
           rule_part.strip!
         end
         rule_part

test/sass/engine_test.rb
@@ -3322,6 +3322,17 @@ CSS
 SASS
   end
 
+  def test_compressed_output_of_nth_selectors
+    assert_equal(<<CSS, render(<<SASS, :syntax => :scss, :style => :compressed))
+:nth-of-type(2n-1),:nth-of-type(2n-1),:nth-of-type(2n-1),:nth-of-type(2n-1),:nth-of-type(2n-1){color:red}:nth-of-type(2n+1),:nth-of-type(2n+1),:nth-of-type(2n+1),:nth-of-type(2n+1),:nth-of-type(2n+1){color:red}
+CSS
+:nth-of-type(2n-1), :nth-of-type(2n-  1), :nth-of-type(2n  -1), :nth-of-type(2n  -  1), :nth-of-type( 2n  -  1 ) {
+  color: red }
+:nth-of-type(2n+1), :nth-of-type(2n+  1), :nth-of-type(2n  +1), :nth-of-type(2n  +  1), :nth-of-type( 2n  +  1 ) {
+  color: red }
+SASS
+  end
+
   private
 
   def assert_hash_has(hash, expected)

commit b2b49cbc972e7853e7166e94703e3bbe3a3cecac

Only compress around dashes within nth selectors.
This addresses a regression introduced in
6e351f0c44f50ed98e79ca68ddac9af80e65bcca and released in Sass 3.4.20.

Fixes #1933.

lib/sass/tree/visitors/to_css.rb
@@ -298,7 +298,10 @@ class Sass::Tree::Visitors::ToCss < Sass::Tree::Visitors::Base
         rule_part = seq.to_s
         if node.style == :compressed
           rule_part.gsub!(/([^,])\s*\n\s*/m, '\1 ')
-          rule_part.gsub!(/\s*([-,+>])\s*/m, '\1')
+          rule_part.gsub!(/\s*([,+>])\s*/m, '\1')
+          rule_part.gsub!(/nth([^( ]*)\(([^)]*)\)/m) do |match|
+            match.tr(" \t\n", "")
+          end
           rule_part.strip!
         end
         rule_part

test/sass/engine_test.rb
@@ -3324,11 +3324,20 @@ SASS
 
   def test_compressed_output_of_nth_selectors
     assert_equal(<<CSS, render(<<SASS, :syntax => :scss, :style => :compressed))
-:nth-of-type(2n-1),:nth-of-type(2n-1),:nth-of-type(2n-1),:nth-of-type(2n-1),:nth-of-type(2n-1){color:red}:nth-of-type(2n+1),:nth-of-type(2n+1),:nth-of-type(2n+1),:nth-of-type(2n+1),:nth-of-type(2n+1){color:red}
+:nth-of-type(2n-1),:nth-child(2n-1),:nth(2n-1),:nth-of-type(2n-1),:nth-of-type(2n-1){color:red}:nth-of-type(2n+1),:nth-child(2n+1),:nth(2n+1),:nth-of-type(2n+1),:nth-of-type(2n+1){color:red}
 CSS
-:nth-of-type(2n-1), :nth-of-type(2n-  1), :nth-of-type(2n  -1), :nth-of-type(2n  -  1), :nth-of-type( 2n  -  1 ) {
+:nth-of-type(2n-1), :nth-child(2n-  1), :nth(2n  -1), :nth-of-type(2n  -  1), :nth-of-type( 2n  -  1 ) {
   color: red }
-:nth-of-type(2n+1), :nth-of-type(2n+  1), :nth-of-type(2n  +1), :nth-of-type(2n  +  1), :nth-of-type( 2n  +  1 ) {
+:nth-of-type(2n+1), :nth-child(2n+  1), :nth(2n  +1), :nth-of-type(2n  +  1), :nth-of-type( 2n  +  1 ) {
+  color: red }
+SASS
+  end
+
+  def test_descendant_selectors_with_leading_dash
+    assert_equal(<<CSS, render(<<SASS, :syntax => :scss, :style => :compressed))
+a -b{color:red}
+CSS
+a -b {
   color: red }
 SASS
   end