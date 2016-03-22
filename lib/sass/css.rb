c22114c2cb010369883b5814670f273d3fd065d4
Chris Eppstein <chris@eppsteins.net>
2016-03-22 11:53:26 -0700

    Enable Style/SingleSpaceBeforeFirstArg.

--- lib/sass/css.rb
+++ lib/sass/css.rb
@@ -75,14 +75,14 @@ module Sass
     # @return [Tree::Node] The root node of the parsed tree
     def build_tree
       root = Sass::SCSS::CssParser.new(@template, @options[:filename], nil).parse
-      parse_selectors    root
-      expand_commas      root
-      nest_seqs          root
-      parent_ref_rules   root
-      flatten_rules      root
-      bubble_subject     root
-      fold_commas        root
-      dump_selectors     root
+      parse_selectors(root)
+      expand_commas(root)
+      nest_seqs(root)
+      parent_ref_rules(root)
+      flatten_rules(root)
+      bubble_subject(root)
+      fold_commas(root)
+      dump_selectors(root)
       root
     end
 
