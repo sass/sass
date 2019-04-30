22e17852835aa5d126c70f7d7459dfbbdca33db9
Teresa Partida <teresapartida.s@gmail.com>
2019-04-30 16:10:48 -0700

    Created .travis.yml file
    Added gitignore
    
    Installed markdown-link-check
    
    Added files for markdown-link-check
    
    Removed unnecessary line
    
    Added glob
    
    Added test for links
    
    Added before-script
    
    Added npm install
    
    Changed npm install for npm ci
    
    Created package.json
    
    Squash commits

---
+++ link-check.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var markdownLinkCheck = require('markdown-link-check');
+var fs = require("fs");
+var glob = require("glob");
+var path = require("path");
+var chalk = require("chalk");
+
+var files = glob.sync("**/*.md", {ignore: ["node_modules/**/*.md"]})
+
+var opts = JSON.parse(fs.readFileSync(".markdown-link-check.json"));
+
+files.forEach(function(file) {
+  var markdown = fs.readFileSync(file).toString();
+  opts.baseUrl = path.basename(__dirname) + "/"
+
+  markdownLinkCheck(markdown, opts, function (err, results) {
+    if (err) {
+        console.error('Error', err);
+        return;
+    }
+
+    console.log(chalk.green("Reading: " + file));
+
+    results.forEach(function (result) {
+      if(result.status === "dead") {
+        if (result.statusCode == 500) {
+          console.log(chalk.yellow("Server error on target: " + result.link));
+        }
+        else {
+          process.exitCode = 1
+          console.log(chalk.red("Dead: " + result.link));
+        }
+      }
+    });
+  });
+});
