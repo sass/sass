7a8b441d1d0b8ec8a54b758b5676873d583d97b9
Natalie Weizenbaum <nweiz@google.com>
2019-05-03 14:00:33 -0700

    More style changes

--- test/toc-check.js
+++ test/toc-check.js
@@ -6,53 +6,55 @@ var fs = require("fs");
 var glob = require("glob");
 var toc = require('markdown-toc');
 
-var files = glob.sync("**/*.md", {ignore: ["node_modules/**/*.md"]})
+var files = glob.sync("**/*.md", {
+  ignore: ["node_modules/**/*.md", "**/*.changes.md"]
+})
 
-//Find the begginning index of the Table of Contents
-function findStartIndex(str) {
-  var startIndex = str.indexOf('Table of Contents');
-  while (str.charAt(startIndex) != '*') startIndex += 1;
-  return startIndex;
+// Returns the index of the first `*` in `markdown`'s table of contents, or
+// `null` if.`markdown` doesn't include a table of contents.
+function findStartIndex(markdown) {
+  var match = markdown.match(/Table of Contents\n*\*/);
+  if (match == null) return null;
+  return match.index + match[0].length - 1; 
 }
 
-//Returns the current Table of Contents
-function getCurrentToc(str, remove) {
-  if (remove === undefined) remove = 0;
-  var startIndex = findStartIndex(str);
-  var endIndex = str.indexOf("## ", startIndex) - 1;
-  var result = str.substring(startIndex, endIndex);
+// Returns the table of contents in `markdown` if it contains one, or `null`
+// otherwise.
+function getCurrentToc(markdown) {
+  var startIndex = findStartIndex(markdown);
+  if (startIndex == null) return null;
 
-  return result.trim();
+  var endIndex = markdown.indexOf("## ", startIndex) - 1;
+  if (endIndex < 0) return null;
+
+  return markdown.substring(startIndex, endIndex).trim();
 }
 
 files.forEach(function(file) {
   var markdown = fs.readFileSync(file).toString();
   console.log("Reading: " + file);
 
-  if (markdown.includes('Table of Contents')) {
-    var generatedToc = toc(markdown, {
-      filter: (string, _, __) => string.indexOf('Table of Contents') === -1,
-      firsth1: false,
-      bullets: "*"
-    }).content;
-    var currentToc = getCurrentToc(markdown);
-
-    if (currentToc !== generatedToc) {
-      var tocDiff = diff.diffLines(currentToc, generatedToc);
-      tocDiff.forEach(function(part) {
-        var color = part.added
-          ? colors.green
-          : (part.removed ? colors.red : colors.grey);
-        process.stderr.write(color(part.value));
-      });
-
-      process.stderr.write('\n');
-
-      process.exitCode = 1;
-    }
-  } else {
+  var currentToc = getCurrentToc(markdown);
+  if (currentToc == null) {
     console.log(colors.yellow("File doesn't have a table of contents"));
+    return;
   }
+
+  var generatedToc = toc(markdown, {
+    filter: (string, _, __) => string.indexOf('Table of Contents') === -1,
+    firsth1: false,
+    bullets: "*"
+  }).content;
+  if (currentToc === generatedToc) return;
+
+  var tocDiff = diff.diffLines(currentToc, generatedToc);
+  tocDiff.forEach(function(part) {
+    var color = part.added
+      ? colors.green
+      : (part.removed ? colors.red : colors.grey);
+    process.stderr.write(color(part.value));
+  });
+  process.stderr.write('\n');
+
+  process.exitCode = 1;
 });
-
-
