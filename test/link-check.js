08eaddb259cc08b68a301e15b4eb81c8481c1cd0
TPartida <tpartida@nearsoft.com>
2019-05-13 15:12:57 -0700

    Fix code style with Standard
    Fixed code style with Standard

--- test/link-check.js
+++ test/link-check.js
@@ -1,35 +1,35 @@
-'use strict';
+'use strict'
 
-var colors = require('colors/safe');
-var fs = require("fs");
-var glob = require("glob");
-var markdownLinkCheck = require('markdown-link-check');
-var path = require("path");
+var colors = require('colors/safe')
+var fs = require('fs')
+var glob = require('glob')
+var markdownLinkCheck = require('markdown-link-check')
+var path = require('path')
 
-var files = glob.sync("**/*.md", {ignore: ["node_modules/**/*.md"]})
+var files = glob.sync('**/*.md', { ignore: ['node_modules/**/*.md'] })
 
-files.forEach(function(file) {
-  var markdown = fs.readFileSync(file).toString();
+files.forEach(function (file) {
+  var markdown = fs.readFileSync(file).toString()
 
   markdownLinkCheck(markdown, {
-    baseUrl: path.basename(__dirname) + "/"
-  }, function(err, results) {
+    baseUrl: path.basename(__dirname) + '/'
+  }, function (err, results) {
     if (err) {
-      console.error('Error', err);
-      return;
+      console.error('Error', err)
+      return
     }
 
-    console.log("Reading: " + file);
+    console.log('Reading: ' + file)
 
-    results.forEach(function(result) {
-      if (result.status === "dead") {
-        if (result.statusCode == 500) {
-          console.log(colors.yellow(`Server error on target: ${result.link}`));
+    results.forEach(function (result) {
+      if (result.status === 'dead') {
+        if (result.statusCode === 500) {
+          console.log(colors.yellow(`Server error on target: ${result.link}`))
         } else {
-          process.exitCode = 1;
-          console.log(colors.red(`Dead: ${result.link}`));
+          process.exitCode = 1
+          console.log(colors.red(`Dead: ${result.link}`))
         }
       }
-    });
-  });
-});
+    })
+  })
+})
--- test/toc-check.js
+++ test/toc-check.js
@@ -1,60 +1,60 @@
-'use strict';
+'use strict'
 
-var colors = require('colors/safe');
-var diff = require('diff');
-var fs = require("fs");
-var glob = require("glob");
-var toc = require('markdown-toc');
+var colors = require('colors/safe')
+var diff = require('diff')
+var fs = require('fs')
+var glob = require('glob')
+var toc = require('markdown-toc')
 
-var files = glob.sync("**/*.md", {
-  ignore: ["node_modules/**/*.md", "**/*.changes.md"]
+var files = glob.sync('**/*.md', {
+  ignore: ['node_modules/**/*.md', '**/*.changes.md']
 })
 
 // Returns the index of the first `*` in `markdown`'s table of contents, or
 // `null` if.`markdown` doesn't include a table of contents.
-function findStartIndex(markdown) {
-  var match = markdown.match(/Table of Contents\n*\*/);
-  if (match == null) return null;
-  return match.index + match[0].length - 1; 
+function findStartIndex (markdown) {
+  var match = markdown.match(/Table of Contents\n*\*/)
+  if (match == null) return null
+  return match.index + match[0].length - 1
 }
 
 // Returns the table of contents in `markdown` if it contains one, or `null`
 // otherwise.
-function getCurrentToc(markdown) {
-  var startIndex = findStartIndex(markdown);
-  if (startIndex == null) return null;
+function getCurrentToc (markdown) {
+  var startIndex = findStartIndex(markdown)
+  if (startIndex == null) return null
 
-  var endIndex = markdown.indexOf("## ", startIndex) - 1;
-  if (endIndex < 0) return null;
+  var endIndex = markdown.indexOf('## ', startIndex) - 1
+  if (endIndex < 0) return null
 
-  return markdown.substring(startIndex, endIndex).trim();
+  return markdown.substring(startIndex, endIndex).trim()
 }
 
-files.forEach(function(file) {
-  var markdown = fs.readFileSync(file).toString();
-  console.log("Reading: " + file);
+files.forEach(function (file) {
+  var markdown = fs.readFileSync(file).toString()
+  console.log('Reading: ' + file)
 
-  var currentToc = getCurrentToc(markdown);
+  var currentToc = getCurrentToc(markdown)
   if (currentToc == null) {
-    console.log(colors.yellow("File doesn't have a table of contents"));
-    return;
+    console.log(colors.yellow("File doesn't have a table of contents"))
+    return
   }
 
   var generatedToc = toc(markdown, {
     filter: (string, _, __) => string.indexOf('Table of Contents') === -1,
     firsth1: false,
-    bullets: "*"
-  }).content;
-  if (currentToc === generatedToc) return;
+    bullets: '*'
+  }).content
+  if (currentToc === generatedToc) return
 
-  var tocDiff = diff.diffLines(currentToc, generatedToc);
-  tocDiff.forEach(function(part) {
+  var tocDiff = diff.diffLines(currentToc, generatedToc)
+  tocDiff.forEach(function (part) {
     var color = part.added
       ? colors.green
-      : (part.removed ? colors.red : colors.grey);
-    process.stderr.write(color(part.value));
-  });
-  process.stderr.write('\n');
+      : (part.removed ? colors.red : colors.grey)
+    process.stderr.write(color(part.value))
+  })
+  process.stderr.write('\n')
 
-  process.exitCode = 1;
-});
+  process.exitCode = 1
+})
