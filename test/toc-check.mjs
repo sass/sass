3e6cf454875567590f0380eafa7492a409fb92a4
Natalie Weizenbaum <nweiz@google.com>
2020-11-23 16:47:31 -0800

    Factor out TOC logic into a shared library

--- test/toc-check.mjs
+++ test/toc-check.mjs
@@ -1,48 +1,20 @@
 import colors from 'colors/safe.js'
 import diff from 'diff'
 import * as fs from 'fs'
-import glob from 'glob'
-import toc from 'markdown-toc'
 
-var files = glob.sync('**/*.md', {
-  ignore: ['node_modules/**/*.md', '**/*.changes.md']
-})
+import * as toc from '../tool/src/toc.mjs'
 
-// Returns the index of the first `*` in `markdown`'s table of contents, or
-// `null` if `markdown` doesn't include a table of contents.
-function findStartIndex (markdown) {
-  var match = markdown.match(/Table of Contents\n*\*/)
-  if (match == null) return null
-  return match.index + match[0].length - 1
-}
-
-// Returns the table of contents in `markdown` if it contains one, or `null`
-// otherwise.
-function getCurrentToc (markdown) {
-  var startIndex = findStartIndex(markdown)
-  if (startIndex == null) return null
-
-  var endIndex = markdown.indexOf('## ', startIndex) - 1
-  if (endIndex < 0) return null
-
-  return markdown.substring(startIndex, endIndex).trim()
-}
-
-files.forEach(function (file) {
+toc.files.forEach(function (file) {
   var markdown = fs.readFileSync(file).toString()
   console.log('Reading: ' + file)
 
-  var currentToc = getCurrentToc(markdown)
+  var currentToc = toc.getCurrent(markdown)
   if (currentToc == null) {
     console.log(colors.yellow("File doesn't have a table of contents"))
     return
   }
 
-  var generatedToc = toc(markdown, {
-    filter: (string, _, __) => string.indexOf('Table of Contents') === -1,
-    firsth1: false,
-    bullets: '*'
-  }).content
+  var generatedToc = toc.generate(markdown)
   if (currentToc === generatedToc) return
 
   var tocDiff = diff.diffLines(currentToc, generatedToc)
