7633bbfe57835ec72d33eb7af616cd5f55f2eb01
awjin <awjinahn@gmail.com>
2021-06-15 09:50:19 -0700

    String API code review.

--- proposal/new-function-and-values-api.d.ts
+++ proposal/new-function-and-values-api.d.ts
@@ -14,16 +14,16 @@
 
 /** ## API */
 
-declare abstract class Value {
-  readonly asList: Value[];
+export abstract class Value {
+  get asList(): Value[];
 
-  readonly hasBrackets: boolean;
+  get hasBrackets(): boolean;
 
-  readonly isTruthy: boolean;
+  get isTruthy(): boolean;
 
-  readonly realNull: Value | null;
+  get realNull(): Value | null;
 
-  readonly separator: ListSeparator;
+  get separator(): ListSeparator;
 
   sassIndexToListIndex(sassIndex: Value, name?: string): number;
 
@@ -44,34 +44,61 @@ declare abstract class Value {
   toString(): string;
 }
 
+/** The JS API representation of a Sass string. */
 export class SassString extends Value {
+  /**
+   * Creates a string with the given `text`:
+   *
+   * - If `options.quotes == true`, set this string's `text` to the semantic
+   *   content of `text`, resolving any escape sequences to their Unicode
+   *   values.
+   * - Otherwise, set this string's `text` to the literal content of `text`,
+   *   preserving the escape sequences as backslashes.
+   * - Set this string's `sassLength` to the number of Unicode code points
+   *   contained in `text`.
+   *
+   *   > Sass internally represents strings as sequences of Unicode
+   *   > [code point]s, whereas JS represents them as UTF-16 [code unit]s.
+   *   > Implementations must convert strings to Unicode code points before
+   *   > setting `text`.
+   *
+   *   [code point]: https://unicode.org/glossary/#code_point
+   *   [code unit]: https://unicode.org/glossary/#code_unit
+   */
+  constructor(
+    text: string,
+    options: {
+      /** @default false */
+      quotes: boolean;
+    }
+  );
+
   /** The contents of the string. */
-  readonly text: string;
+  get text(): string;
 
   /** Whether the string has quotes. */
-  readonly hasQuotes: boolean;
+  get hasQuotes(): boolean;
 
-  /** The Unicode-code-point-aware length of the string. */
-  readonly sassLength: number;
-
-  /**
-   * Creates a string with the given `text`.
-   *
-   * @param [options.quotes = false]
-   */
-  constructor(text: string, options: {quotes: boolean});
+  /** The number of Unicode code points in the string. */
+  get sassLength(): number;
 
   /**
-   * Converts `sassIndex` to a JS-style index into `text`.
+   * Converts the Sass index `sassIndex` to a JS index into `text`:
    *
-   * Given a Sass index `index`, perform the following procedure:
-   * - If `sassIndex` isn't an integer or a valid index for this string, throw
+   * - If `sassIndex` isn't an integer, throw an `Exception`.
+   * - If `sassIndex == 0`, throw an `Exception`.
+   * - If the absolute value of `sassIndex` is greater than `sassLength`, throw
    *   an `Exception`.
-   * - Make `index` refer to UTF-16 code units, since Sass indices refer to
-   *   Unicode code points.
-   * - Convert `index` from one-based to zero-based.
-   *   - If `index` is negative, it is indexing from the end of the string.
-   *     Convert this to the equivalent positive index.
+   * - Sass indices start counting at 1, and may be negative in order to index
+   *   from the end of the string. Let `normalizedIndex` be equivalent to a Sass
+   *   index, except it starts counting at 0 and does not support negative
+   *   values.
+   *   - If `sassIndex > 0`, set `normalizedIndex = sassIndex - 1`.
+   *   - Otherwise, if `sassIndex < 0`, set `normalizedIndex = sassLength + sassIndex`.
+   * - Sass indices count Unicode code points, whereas JS indices count UTF-16
+   *   code units. Let `jsIndex` be a JS index. Set `jsIndex` to the first code
+   *   unit of the Unicode code point that `normalizedIndex` points to.
+   * - Return `jsIndex`.
    */
   sassIndexToStringIndex(sassIndex: Value): number;
 }
