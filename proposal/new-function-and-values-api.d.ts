d19227c819f03abfcb45643f2dd3cf968a524517
awjin <awjinahn@gmail.com>
2021-06-22 11:20:11 -0700

    Code review: colors and strings.

--- proposal/new-function-and-values-api.d.ts
+++ proposal/new-function-and-values-api.d.ts
@@ -14,6 +14,13 @@
 
 /** ## API */
 
+/**
+ * The JS API representation of a Sass value.
+ *
+ * > To make the spec terser and easier to author, each subclass that extends
+ * > Value has a virtual, private property named `internal` that refers to the
+ * > Sass value it is representing.
+ */
 export abstract class Value {
   get asList(): Value[];
 
@@ -44,22 +51,37 @@ export abstract class Value {
   toString(): string;
 }
 
-/** The JS API representation of a Sass color. */
+/**
+ * The JS API representation of a Sass color.
+ *
+ * `internal` refers to a Sass number.
+ */
 export class SassColor extends Value {
   /**
    * Creates an RGB color:
    *
-   * - If `red` is not between `0` and `255` inclusive, throw an `Exception`.
-   * - If `green` is not between `0` and `255` inclusive, throw an `Exception`.
-   * - If `blue` is not between `0` and `255` inclusive, throw an `Exception`.
-   * - If `alpha` is not between `0` and `1` inclusive, throw an `Exception`.
-   * - Let `color` be a new `SassColor`:
-   *   - Set `color`'s `red` channel to `red`.
-   *   - Set `color`'s `green` channel to `green`.
-   *   - Set `color`'s `blue` channel to `blue`.
-   *   - If `alpha` was passed, set `color`'s `alpha` channel to `alpha`.
-   *   - Otherwise, set `color`'s `alpha` channel to 1.
-   * - Return `color`.
+   * - Let `sassRed` be a Sass number with a value of `red` `fuzzyRound`ed to
+   *   the nearest integer.
+   *
+   * - Let `sassGreen` be a Sass number with a value of `green` `fuzzyRound`ed
+   *   to the nearest integer
+   *
+   * - Let `sassBlue` be a Sass number with a value of `blue` `fuzzyRound`ed to
+   *   the nearest integer.
+   *
+   * - If `alpha` was passed, let `sassAlpha` be a Sass number with a value of
+   *   `alpha`.
+   *
+   * - Set `internal` to the result of running [`rgb()`] with the following
+   *   inputs:
+   *   - $red set to `sassRed`
+   *   - $green set to `sassGreen`
+   *   - $blue set to `sassBlue`
+   *   - If `alpha` was passed, $alpha set to `sassAlpha`
+   *
+   *   [`rgb()`]: ../spec/functions.md#rgb-and-rgba
+   *
+   * - Return this color.
    */
   static rgb(
     red: number,
@@ -71,18 +93,25 @@ export class SassColor extends Value {
   /**
    * Creates an HSL color:
    *
-   * - If `saturation` is not between `0` and `100` inclusive, throw an
-   *   `Exception`.
-   * - If `lightness` is not between `0` and `100` inclusive, throw an
-   *   `Exception`.
-   * - If `alpha` is not between `0` and `1` inclusive, throw an `Exception`.
-   * - Let `color` be a new `SassColor`:
-   *   - Set `color`'s `hue` to `hue % 360`.
-   *   - Set `color`'s `saturation` to `saturation`.
-   *   - Set `color`'s `lightness` to `lightness`.
-   *   - If `alpha` was passed, set `color`'s `alpha` channel to `alpha`.
-   *   - Otherwise, set `color`'s `alpha` channel to 1.
-   * - Return `color`.
+   * - Let `sassHue` be a Sass number with a value of `hue`.
+   *
+   * - Let `sassSaturation` be a Sass number with a value of `saturation`.
+   *
+   * - Let `sassLightness` be a Sass number with a value of `lightness`.
+   *
+   * - If `alpha` was passed, let `sassAlpha` be a Sass number with a value of
+   *   `alpha`.
+   *
+   * - Set `internal` to the result of running [`hsl()`] with the following
+   *   inputs:
+   *   - $hue set to `sassHue`
+   *   - $saturation set to `sassSaturation`
+   *   - $lightness set to `sassLightness`
+   *   - If `alpha` was passed, $alpha set to `sassAlpha`
+   *
+   *   [`hsl()`]: ../spec/functions.md#hsl-and-hsla
+   *
+   * - Return this color.
    */
   static hsl(
     hue: number,
@@ -94,18 +123,25 @@ export class SassColor extends Value {
   /**
    * Creates an HWB color:
    *
-   * - If `whiteness` is not between `0` and `100` inclusive, throw an
-   *   `Exception`.
-   * - If `blackness` is not between `0` and `100` inclusive, throw an
-   *   `Exception`.
-   * - If `alpha` is not between `0` and `1` inclusive, throw an `Exception`.
-   * - Let `color` be a new `SassColor`.
-   *   - Set `color`'s `hue` to `hue % 360`.
-   *   - Set `color`'s `whiteness` to `whiteness`.
-   *   - Set `color`'s `blackness` to `blackness`.
-   *   - If `alpha` was passed, set `color`'s `alpha` channel to `alpha`.
-   *   - Otherwise, set `color`'s `alpha` channel to 1.
-   * - Return `color`.
+   * - Let `sassHue` be a Sass number with a value of `hue`.
+   *
+   * - Let `sassWhiteness` be a Sass number with a value of `whiteness`.
+   *
+   * - Let `sassBlackness` be a Sass number with a value of `blackness`.
+   *
+   * - If `alpha` was passed, let `sassAlpha` be a Sass number with a value of
+   *   `alpha`.
+   *
+   * - Set `internal` to the result of running [`hwb()`] with the following
+   *   inputs:
+   *   - $hue set to `sassHue`
+   *   - $whiteness set to `sassWhiteness`
+   *   - $blackness set to `sassBlackness`
+   *   - If `alpha` was passed, $alpha set to `sassAlpha`
+   *
+   *   [`hwb()`]: ../spec/color.md#hwb
+   *
+   * - Return this color.
    */
   static hwb(
     hue: number,
@@ -114,31 +150,31 @@ export class SassColor extends Value {
     alpha?: number
   ): SassColor;
 
-  /** This color's red channel. */
+  /** `internal`'s red channel. */
   get red(): number;
 
-  /** This color's green channel. */
+  /** `internal`'s green channel. */
   get green(): number;
 
-  /** This color's blue channel. */
+  /** `internal`'s blue channel. */
   get blue(): number;
 
-  /** This color's hue. */
+  /** `internal`'s hue. */
   get hue(): number;
 
-  /** This color's saturation. */
+  /** `internal`'s saturation. */
   get saturation(): number;
 
-  /** This color's lightness. */
+  /** `internal`'s lightness. */
   get lightness(): number;
 
-  /** This color's whiteness. */
+  /** `internal`'s whiteness. */
   get whiteness(): number;
 
-  /** This color's blackness. */
+  /** `internal`'s blackness. */
   get blackness(): number;
 
-  /** This color's alpha channel. */
+  /** `internal`'s alpha channel. */
   get alpha(): number;
 
   /**
@@ -146,14 +182,19 @@ export class SassColor extends Value {
    * channels:
    *
    * - Let `oldColor` be this color.
+   *
    * - If `red` was passed, let `newRed = red`.
    * - Otherwise, let `newRed = oldColor.red`.
+   *
    * - If `green` was passed, let `newGreen = green`.
    * - Otherwise, let `newGreen = oldColor.green`.
+   *
    * - If `blue` was passed, let `newBlue = blue`.
    * - Otherwise, let `newBlue = oldColor.blue`.
+   *
    * - If `alpha` was passed, let `newAlpha = alpha`.
    * - Otherwise, let `newAlpha = oldColor.alpha`.
+   *
    * - Return the result of
    *   `SassColor.rgb(newRed, newGreen, newBlue, newAlpha)`.
    */
@@ -169,14 +210,19 @@ export class SassColor extends Value {
    * values:
    *
    * - Let `oldColor` be this color.
+   *
    * - If `hue` was passed, let `newHue = hue`.
    * - Otherwise, let `newHue = oldColor.hue`.
+   *
    * - If `saturation` was passed, let `newSaturation = saturation`.
    * - Otherwise, let `newSaturation = oldColor.saturation`.
+   *
    * - If `lightness` was passed, let `newLightness = lightness`.
    * - Otherwise, let `newLightness = oldColor.lightness`.
+   *
    * - If `alpha` was passed, let `newAlpha = alpha`.
    * - Otherwise, let `newAlpha = oldColor.alpha`.
+   *
    * - Return the result of
    *   `SassColor.hsl(newHue, newSaturation, newLightness, newAlpha)`.
    */
@@ -192,14 +238,19 @@ export class SassColor extends Value {
    * values:
    *
    * - Let `oldColor` be this color.
+   *
    * - If `hue` was passed, let `newHue = hue`.
    * - Otherwise, let `newHue = oldColor.hue`.
+   *
    * - If `whiteness` was passed, let `newWhiteness = whiteness`.
    * - Otherwise, let `newWhiteness = oldColor.whiteness`.
+   *
    * - If `blackness` was passed, let `newBlackness = blackness`.
    * - Otherwise, let `newBlackness = oldColor.blackness`.
+   *
    * - If `alpha` was passed, let `newAlpha = alpha`.
    * - Otherwise, let `newAlpha = oldColor.alpha`.
+   *
    * - Return the result of
    *   `SassColor.hwb(newHue, newWhiteness, newBlackness, newAlpha)`.
    */
@@ -214,22 +265,19 @@ export class SassColor extends Value {
   changeAlpha(alpha: number): SassColor;
 }
 
-/** The JS API representation of a Sass string. */
+/**
+ * The JS API representation of a Sass string.
+ *
+ * `internal` refers to a Sass string.
+ */
 export class SassString extends Value {
   /**
-   * > This virtual property exists only to make the spec more concise and
-   * > precise.
+   * Creates a Sass string:
    *
-   * An object that behaves like a Sass string instance.
+   * - Set `internal` to a Sass string with the same contents as `text` and
+   *   quoted value equal to `options.quotes`.
    *
-   * internalString: Value;
-   */
-
-  /**
-   * - If `options.quotes === true`, set `internalString`'s contents to `text`.
-   * - Otherwise, let `escapedText` be `text` with all of its escape sequences
-   *   preserved as literal backslashes. Set `internalString`'s contents to
-   *   `escapedText`.
+   * - Return this string.
    */
   constructor(
     text: string,
@@ -239,10 +287,10 @@ export class SassString extends Value {
     }
   );
 
-  /** The contents of `internalString`. */
+  /** The contents of `internal` serialized as UTF-16 code units. */
   get text(): string;
 
-  /** Whether `internalString` has quotes. */
+  /** Whether `internal` has quotes. */
   get hasQuotes(): boolean;
 
   /** The number of Unicode code points in `text`. */
@@ -251,7 +299,7 @@ export class SassString extends Value {
   /**
    * Converts the Sass index `sassIndex` to a JS index into `text`:
    *
-   * - If `sassIndex` is not a Sass number, throw an exception.
+   * - If `sassIndex` is not a unitless Sass number, throw an exception.
    *
    * - Let `value` be the value of `sassIndex`. Let `index` be the result of
    *   `fuzzyAsInt(value)`. If `index` is null, throw an `Exception`.
@@ -261,16 +309,16 @@ export class SassString extends Value {
    * - If the absolute value of `index` is greater than `sassLength`, throw
    *   an `Exception`.
    *
-   * > Sass indices start counting at 1, and may be negative in order to index
-   * > from the end of the string.
+   *   > Sass indices start counting at 1, and may be negative in order to index
+   *   > from the end of the string.
    *
    * - If `index > 0`, let `normalizedIndex = index - 1`.
    * - Otherwise, if `index < 0`, let `normalizedIndex = sassLength + index`.
    *
-   * > Sass indices count Unicode [code point]s, whereas JS indices count UTF-16
-   * > [code units].
-   * > [code point]: https://unicode.org/glossary/#code_point
-   * > [code unit]: https://unicode.org/glossary/#code_unit
+   *   > Sass indices count Unicode [code point]s, whereas JS indices count
+   *   > UTF-16 [code units].
+   *   > [code point]: https://unicode.org/glossary/#code_point
+   *   > [code unit]: https://unicode.org/glossary/#code_unit
    *
    * - Let `jsIndex` be a JS index. Set `jsIndex` to the first code
    *   unit of the Unicode code point that `normalizedIndex` points to.
