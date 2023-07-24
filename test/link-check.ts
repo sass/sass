a4da3ac21ffc2447ca47d6f3ab0566698808a51f
James Stuckey Weber <jamesnw@gmail.com>
2023-07-24 10:09:52 -0400

    Temporarily add debug info to link check

--- test/link-check.ts
+++ test/link-check.ts
@@ -71,7 +71,7 @@ function verifyLinkCheckResults(
             colors.yellow(`Server error on target: ${result.link}`)
           );
         } else {
-          flagDeadLink(result.link);
+          flagDeadLink(`${result.link} ${result.status} ${result.err}`);
         }
         break;
 
